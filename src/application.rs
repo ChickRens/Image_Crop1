0ad37a36e2d48d79e76db95a50b3df80de3cad5a
ChickRen <chiaking207@icloud.com>
2026-05-08 18:17:39 +0900

    refactor: Remove repository module and ImageRepository trait

--- src/application.rs
+++ src/application.rs
@@ -1,5 +1,4 @@
 pub mod errors;
 pub mod interface;
-pub mod repository;
 pub mod types;
 pub mod usecase;
--- src/application/repository/image_repository.rs
+++
@@ -1,7 +0,0 @@
-use crate::application::types::image::Image;
-use crate::domain::value_object::image_id::ImageId;
-
-pub trait ImageRepository {
-    fn save(&mut self, image: Image, image_id: ImageId);
-    fn get(&self, image_id: &ImageId) -> Option<Image>;
-}
