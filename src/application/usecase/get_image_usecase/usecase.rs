dceda1812ba08a983c25f18e608b8dc59d3fad60
ChickRen <chiaking207@icloud.com>
2026-05-08 20:05:55 +0900

    refactor: Simplify GetImageUseCase by removing ImageMetaRepository

--- src/application/usecase/get_image_usecase/usecase.rs
+++ src/application/usecase/get_image_usecase/usecase.rs
@@ -5,37 +5,30 @@ use crate::application::errors::validation_errors::ValidationErrors;
 use crate::application::repository::image_repository::ImageRepository;
 use crate::application::usecase::get_image_usecase::get_image_input::GetImageInput;
 use crate::application::usecase::get_image_usecase::get_image_output::GetImageOutput;
-use crate::domain::repository::image_meta_repository::ImageMetaRepository;
+use crate::domain::repository::image_meta_repository::ImageRepository;
 use crate::domain::repository::session_repository::SessionRepository;
 
-
-
-pub struct GetImageUseCase<SR ,IR, MR>
+pub struct GetImageUseCase<SR ,IR>
 where
     SR: SessionRepository,
-    IR: ImageRepository,
-    MR: ImageMetaRepository
+    IR: ImageRepository
 {
     session_repo: SR,
     image_repo: IR,
-    meta_repo: MR,
 }
 
-impl<SR, IR, MR> GetImageUseCase<SR, IR, MR>
+impl<SR, IR> GetImageUseCase<SR, IR>
 where
     SR: SessionRepository,
     IR: ImageRepository,
-    MR: ImageMetaRepository,
 {
     pub fn new(
         session_repository: SR,
         image_repository: IR,
-        image_meta_repository: MR,
     ) -> Self {
         Self {
             session_repo: session_repository,
             image_repo: image_repository,
-            meta_repo: image_meta_repository,
         }
     }
 
