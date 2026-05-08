aa0d0b3f2b025bfc5c70f10863b754719474e3d4
ChickRen <chiaking207@icloud.com>
2026-05-08 20:01:53 +0900

    refactor: Update ImageRepository to simplify image saving logic

--- src/domain/repository/image_meta_repository.rs
+++ src/domain/repository/image_meta_repository.rs
@@ -2,6 +2,6 @@ use crate::domain::entity::image::Image;
 use crate::domain::value_object::image_id::ImageId;
 
 pub trait ImageRepository {
-    fn save(&mut self, meta: Image);
+    fn save(&mut self, image: Image);
     fn get(&self, image_id: &ImageId) -> Option<Image>;
 }
