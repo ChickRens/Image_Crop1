066b2b7ec776b0d480988982b3a0a6a8f8094430
ChickRen <chiaking207@icloud.com>
2026-05-10 21:38:23 +0900

    fix: Change return type of segment method in ImageSegmenter trait to SegmentedImage

--- src/application/interface/image_segmenter.rs
+++ src/application/interface/image_segmenter.rs
@@ -3,5 +3,5 @@ use crate::application::types::segmented_image::SegmentedImage;
 use crate::domain::value_object::point::Point;
 
 pub trait ImageSegmenter {
-    fn segment(&self, image:Image, points: Option<&Point>) -> &SegmentedImage;
+    fn segment(&self, image:Image, points: Option<&Point>) -> SegmentedImage;
 }
