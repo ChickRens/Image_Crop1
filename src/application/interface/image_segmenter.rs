4383a4c2a754322585d7a25382bc26539f6b0344
ChickRen <chiaking207@icloud.com>
2026-05-12 20:39:25 +0900

    feat: Update ImageSegmenter trait to return Result with SegmentationErrors

--- src/application/interface/image_segmenter.rs
+++ src/application/interface/image_segmenter.rs
@@ -1,7 +1,12 @@
+use crate::application::errors::segmentation_error::SegmentationErrors;
 use crate::domain::entity::image::Image;
 use crate::application::types::segmented_image::SegmentedImage;
 use crate::domain::value_object::point::Point;
 
 pub trait ImageSegmenter {
-    fn segment(&self, image:Image, points: Option<&Point>) -> SegmentedImage;
+    fn segment(&self, image:Image, points: Option<&Point>) -> Result<SegmentedImage,SegmentationErrors>;
+}
+
+pub trait ImageSegmenterPreparing {
+    fn prepare(&mut self, image: Image) -> Result<(), SegmentationErrors>;
 }
