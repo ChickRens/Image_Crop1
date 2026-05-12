a4fe3c94c010b53f3e7b7b33cbf833d4dc39a82a
ChickRen <chiaking207@icloud.com>
2026-05-12 22:03:32 +0900

    fix: Update get method in ContextRepository trait to return Self::Context type

--- src/application/interface/context_repository.rs
+++ src/application/interface/context_repository.rs
@@ -1,10 +1,8 @@
-use std::task::Context;
-
 use crate::application::types::crop_id::SegmentId;
 
 pub trait ContextRepository {
     type Context;
 
     fn save(&mut self, context: Self::Context);
-    fn get(&self, segment_id: SegmentId) -> Context;
+    fn get(&self, segment_id: SegmentId) -> Self::Context;
 }
