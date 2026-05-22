0c7319ce71b3d8ab513b927ad45ebd7224fc6ac5
ChickRen <chiaking207@icloud.com>
2026-05-22 21:47:25 +0900

    fix: update get method to accept a reference to SegmentId

--- src/application/interface/context_repository.rs
+++ src/application/interface/context_repository.rs
@@ -3,6 +3,6 @@ use crate::application::types::segment_id::SegmentId;
 pub trait ContextRepository {
     type Context;
 
-    fn save(&mut self, context: Self::Context);
-    fn get(&self, segment_id: SegmentId) -> Self::Context;
+    fn save(&mut self, context: Self::Context) ;
+    fn get(&self, segment_id: &SegmentId) -> Option<Self::Context>;
 }
