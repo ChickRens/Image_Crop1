72a4ca3a748486439697d1d874e23d4cd1b4ea72
ChickRen <chiaking207@icloud.com>
2026-05-27 23:05:47 +0900

    feat: update Mask struct to use Array4 for enhanced dimensionality support

--- src/application/types/mask.rs
+++ src/application/types/mask.rs
@@ -1,14 +1,24 @@
-use ndarray::Array2;
+use ndarray::Array4;
+use ndarray::ArrayView4;
 use ndarray::OwnedRepr;
 use ndarray::prelude::ArrayBase;
 use ndarray::prelude::Dim;
 
 pub struct Mask {
-    value: Array2<f32>
+    value: Array4<f32>,
 }
 
 impl Mask {
-    pub fn new(value:ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>, f32> ) -> Self {
+    pub fn new(value: ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>, f32>) -> Self {
         Self { value }
     }
+
+    pub fn view(&self) -> ArrayView4<f32> {
+        self.value.view()
+    }
+
+    pub fn into_mask(self) -> ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>, f32> {
+        self.value
+    }
+    // for Zero-Copy
 }
