68b8d78b89f00eafcd92aaedefdcb28fbb620212
ChickRen <chiaking207@icloud.com>
2026-05-27 22:41:23 +0900

    feat: refactor SAM2 segmenter and data structures for improved context handling

--- src/infrastructure/segmenter/sam2.rs
+++ src/infrastructure/segmenter/sam2.rs
@@ -1,15 +1,20 @@
 use crate::application::errors::segmentation_error::SegmentationErrors;
 use crate::application::interface::image_segmenter::{ImageSegmenter, ImageSegmenterPreparing};
-use crate::application::types::segmented_image::SegmentedImage;
+use crate::application::types::mask::Mask as ApplicationMask;
 use crate::domain::entity::image::Image;
-use crate::domain::value_object::point::Point;
-use image::DynamicImage;
-use ndarray::{s, Array, Array4, IxDyn};
+use crate::infrastructure::segmenter::sam2_data::{
+    DenseEmbeddings, HighResFeatureS0, HighResFeatureS1, ImageEmbeddings, Mask,
+    SAM2InferenceContext, SAM2Inputs, SAM2StaticContext, SparseEmbeddings,
+};
+use image::{DynamicImage, GenericImageView, Pixel};
+use ndarray::prelude::{ArrayBase, Dim};
+use ndarray::{Array, Array3, Array4, ArrayView4, Ix3, Ix4, OwnedRepr, ViewRepr, s};
 use ort::session::builder::GraphOptimizationLevel;
-use ort::session::Session;
-use ort::value::{DynValueTypeMarker, Tensor, Value};
+use ort::session::{Session, SessionOutputs};
+use ort::value::{
+    DynValueTypeMarker, PrimitiveTensorElementType, TensorRef, TensorValueType, Value,
+};
 use std::collections::HashMap;
-use std::sync::Arc;
 
 type OrtResult<T> = Result<T, Box<dyn std::error::Error>>;
 
@@ -19,7 +24,6 @@ pub struct Sam2Segmenter {
     encoder_session: Session,
     prompt_encoder_session: Session,
     mask_decoder_session: Session,
-    image_embedding: Option<Arc<Array<f32, IxDyn>>>,
 }
 
 impl Sam2Segmenter {
@@ -27,10 +31,18 @@ impl Sam2Segmenter {
         ort::init();
 
         Ok(Self {
-            encoder_session: Self::build_session(format!("{}/sam2.1_hiera_small_encoder.onnx", model_dir))?,
-            prompt_encoder_session: Self::build_session(format!("{}/sam2.1_hiera_small_prompt_encoder.onnx", model_dir))?,
-            mask_decoder_session: Self::build_session(format!("{}/sam2.1_hiera_small_mask_decoder.onnx", model_dir))?,
-            image_embedding: None,
+            encoder_session: Self::build_session(format!(
+                "{}/sam2.1_hiera_small_encoder.onnx",
+                model_dir
+            ))?,
+            prompt_encoder_session: Self::build_session(format!(
+                "{}/sam2.1_hiera_small_prompt_encoder.onnx",
+                model_dir
+            ))?,
+            mask_decoder_session: Self::build_session(format!(
+                "{}/sam2.1_hiera_small_mask_decoder.onnx",
+                model_dir
+            ))?,
         })
     }
 
@@ -40,161 +52,267 @@ impl Sam2Segmenter {
             .commit_from_file(path)?)
     }
 
-    fn array_to_value(array: &Array<f32, IxDyn>) -> OrtResult<Value<DynValueTypeMarker>> {
-        let shape = array.shape().iter().map(|&x| x as i64).collect::<Vec<_>>();
-        let data = array.iter().cloned().collect::<Vec<_>>();
-        Ok(Value::from(Tensor::from_array((shape.as_slice(), data))?))
+    fn _tensor_to_array4<OutputType>(onnx_output: &SessionOutputs, name: &str) -> Array4<OutputType>
+    where
+        OutputType: PrimitiveTensorElementType + Clone,
+    {
+        let (shape, data) = onnx_output[name]
+            .try_extract_tensor::<OutputType>()
+            .unwrap();
+
+        let dims: Vec<usize> = shape.iter().map(|x| *x as usize).collect();
+
+        Array4::from_shape_vec(Ix4(dims[0], dims[1], dims[2], dims[3]), data.to_vec()).unwrap()
     }
 
-    fn zero_value(shape: &[i64]) -> OrtResult<Value<DynValueTypeMarker>> {
-        let size = shape.iter().product::<i64>() as usize;
-        let data = vec![0.0; size];
-        Ok(Value::from(Tensor::from_array((shape, data))?))
+    fn _tensor_to_array3<OutputType>(onnx_output: &SessionOutputs, name: &str) -> Array3<OutputType>
+    where
+        OutputType: PrimitiveTensorElementType + Clone,
+    {
+        let (shape, data) = onnx_output[name]
+            .try_extract_tensor::<OutputType>()
+            .unwrap();
+
+        let dims: Vec<usize> = shape.iter().map(|x| *x as usize).collect();
+
+        Array3::from_shape_vec(Ix3(dims[0], dims[1], dims[2]), data.to_vec()).unwrap()
     }
 
-    fn run_session(session: &mut Session, inputs: OrtInputs) -> OrtResult<Array<f32, IxDyn>> {
-        let outputs = session.run(inputs)?;
-        let (_, value) = outputs
-            .into_iter()
-            .next()
-            .ok_or("no outputs in the session result")?;
-        let (shape, data) = value.try_extract_tensor::<f32>()?;
-        let shape = shape.iter().map(|&x| x as usize).collect::<Vec<usize>>();
-        Ok(Array::from_shape_vec(IxDyn(&shape), data.to_vec())?)
-    }
+    fn _encode_image(
+        &mut self,
+        original_image: &DynamicImage,
+    ) -> ort::Result<(ImageEmbeddings, HighResFeatureS0, HighResFeatureS1)> {
+        let resized_image =
+            original_image.resize_exact(1024, 1024, image::imageops::FilterType::CatmullRom);
 
-    pub fn encode_image(&mut self, img: &DynamicImage) -> OrtResult<Array<f32, IxDyn>> {
-        let img = img.resize_exact(1024, 1024, image::imageops::FilterType::Lanczos3);
-        let img = img.to_rgb8();
-        let mut tensor_data = Vec::with_capacity(1024 * 1024 * 3);
+        let mut input: ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>, f32> =
+            Array::zeros((1, 3, 1024, 1024));
+        for pixel in resized_image.pixels() {
+            let x = pixel.0 as usize;
+            let y = pixel.1 as usize;
+            let channels = pixel.2.channels();
+            let r: u8 = channels[0];
+            let g: u8 = channels[1];
+            let b: u8 = channels[2];
 
-        for pixel in img.pixels() {
-            let r = pixel[0] as f32 / 255.0;
-            let g = pixel[1] as f32 / 255.0;
-            let b = pixel[2] as f32 / 255.0;
-            tensor_data.push((r - 0.485) / 0.229);
-            tensor_data.push((g - 0.456) / 0.224);
-            tensor_data.push((b - 0.406) / 0.225);
+            input[[0, 0, y, x]] = (r as f32) / 255.0;
+            input[[0, 1, y, x]] = (g as f32) / 255.0;
+            input[[0, 2, y, x]] = (b as f32) / 255.0;
         }
 
-        let input_tensor = Array4::from_shape_vec((1, 3, 1024, 1024), tensor_data)?;
-        let input_name = {
-            let inputs = self.encoder_session.inputs();
-            inputs[0].name().to_string()
+        let input_value: Value<TensorValueType<f32>> = Value::from_array(input)?;
+        let outputs: &SessionOutputs = &self
+            .encoder_session
+            .run(ort::inputs!["image" => input_value])?;
+
+        // 出力を取得
+        let image_embeddings_value: ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>, f32> =
+            Self::_tensor_to_array4::<f32>(outputs, "image_embeddings");
+        let high_res_feature_s0_value: ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>, f32> =
+            Self::_tensor_to_array4::<f32>(outputs, "high_res_feature_s0");
+        let high_res_feature_s1_value: ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>, f32> =
+            Self::_tensor_to_array4::<f32>(outputs, "high_res_feature_s1");
+
+        let image_embeddings = ImageEmbeddings::new(image_embeddings_value);
+        let high_res_feature_s0 = HighResFeatureS0::new(high_res_feature_s0_value);
+        let high_res_feature_s1 = HighResFeatureS1::new(high_res_feature_s1_value);
+
+        Ok((image_embeddings, high_res_feature_s0, high_res_feature_s1))
+    }
+
+    fn _encode_prompt(
+        &mut self,
+        points_coords: Vec<(f32, f32)>,
+        points_labels: Vec<i64>,
+        mask: Option<ArrayView4<f32>>,
+    ) -> ort::Result<(SparseEmbeddings, DenseEmbeddings)> {
+        // name: points_coords
+        // tensor: float32[1,-1,2]
+        // points_labels
+        // name: points_labels
+        // tensor: int64[1,-1]
+        // masks
+        // name: masks
+        // tensor: float32[1,1,256,256]
+
+        let mut input_coords: ArrayBase<OwnedRepr<f32>, Dim<[usize; 3]>, f32> =
+            Array::zeros((1, points_coords.len(), 2));
+        for (i, coord) in points_coords.iter().enumerate() {
+            input_coords[[0, i, 0]] = coord.0;
+            input_coords[[0, i, 1]] = coord.1;
+        }
+
+        let mut input_labels: ArrayBase<OwnedRepr<i64>, Dim<[usize; 2]>, i64> =
+            Array::zeros((1, points_labels.len()));
+        for (i, label) in points_labels.iter().enumerate() {
+            input_labels[[0, i]] = *label;
+        }
+
+        let none_binding: ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>, f32> =
+            Array::zeros((1, 1, 256, 256));
+        let input_mask: ArrayBase<ViewRepr<&f32>, Dim<[usize; 4]>, f32> = match mask {
+            Some(mask) => mask,
+            None => none_binding.view(),
         };
-        let shape = vec![1i64, 3, 1024, 1024];
-        let tensor = Tensor::from_array((shape.as_slice(), input_tensor.iter().cloned().collect::<Vec<_>>()))?;
-        let mut inputs = OrtInputs::new();
-        inputs.insert(input_name, Value::from(tensor));
 
-        Self::run_session(&mut self.encoder_session, inputs)
+        let outputs = &self.encoder_session.run(ort::inputs![
+                "points_coords" => TensorRef::from_array_view(&input_coords)?,
+                "points_labels" => TensorRef::from_array_view(&input_labels)?,
+                "mask"          => TensorRef::from_array_view(input_mask)?])?;
+
+        let sparse_embeddings_value: ArrayBase<OwnedRepr<f32>, Dim<[usize; 3]>, f32> =
+            Self::_tensor_to_array3::<f32>(outputs, "sparse_embeddings");
+        let dense_embeddings_value: ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>, f32> =
+            Self::_tensor_to_array4::<f32>(outputs, "dense_embeddings");
+
+        let sparse_embeddings = SparseEmbeddings::new(sparse_embeddings_value);
+        let dense_embeddings = DenseEmbeddings::new(dense_embeddings_value);
+
+        Ok((sparse_embeddings, dense_embeddings))
     }
 
-    pub fn encode_prompts(
+    fn _decode_mask(
         &mut self,
-        points: &[Point],
-        orig_h: f32,
-        orig_w: f32,
-    ) -> OrtResult<(Array<f32, IxDyn>, Array<f32, IxDyn>)> {
-        let num_points = points.len();
-        let coords: Vec<f32> = points
-            .iter()
-            .flat_map(|point| {
-                let x_norm = point.coordinate().x() as f32 / orig_w;
-                let y_norm = point.coordinate().y() as f32 / orig_h;
-                [x_norm * 1024.0, y_norm * 1024.0]
-            })
-            .collect();
+        image_embeddings: &ImageEmbeddings,
+        sparse_embeddings: &SparseEmbeddings,
+        dense_embeddings: &DenseEmbeddings,
+        high_res_feature_s0: &HighResFeatureS0,
+        high_res_feature_s1: &HighResFeatureS1,
+    ) -> ort::Result<Mask> {
+        // Input
 
-        let labels: Vec<f32> = points
-            .iter()
-            .map(|point| match point.label() {
-                crate::domain::value_object::point::PointLabel::BACKGROUND => 0.0,
-                crate::domain::value_object::point::PointLabel::FOREGROUND => 1.0,
-            })
-            .collect();
+        // image_embeddings
+        // name: image_embeddings
+        // tensor: float32[1,256,64,64]
 
-        let mut inputs = OrtInputs::new();
-        inputs.insert(
-            "points_coords".to_string(),
-            Self::array_to_value(&Array::from_shape_vec(IxDyn(&[1, num_points, 2]), coords)?)?,
-        );
-        inputs.insert(
-            "points_labels".to_string(),
-            Self::array_to_value(&Array::from_shape_vec(IxDyn(&[1, num_points]), labels)?)?,
-        );
-        inputs.insert("boxes".to_string(), Self::zero_value(&[1, 4])?);
-        inputs.insert("masks".to_string(), Self::zero_value(&[1, 1, 1024, 1024])?);
+        // image_pe
+        // name: image_pe
+        // tensor: float32[1,256,64,64]
 
-        let outputs = self.prompt_encoder_session.run(inputs)?;
-        let sparse_emb = outputs[0].try_extract_tensor::<f32>()?;
-        let dense_emb = outputs[1].try_extract_tensor::<f32>()?;
+        // sparse_prompt_embeddings
+        // name: sparse_prompt_embeddings
+        // tensor: float32[1,4,256]
 
-        let sparse_array = Array::from_shape_vec(
-            IxDyn(&sparse_emb.0.iter().map(|&x| x as usize).collect::<Vec<_>>() ),
-            sparse_emb.1.to_vec(),
-        )?;
+        // dense_prompt_embeddings
+        // name: dense_prompt_embeddings
+        // tensor: float32[1,256,64,64]
 
-        let dense_array = Array::from_shape_vec(
-            IxDyn(&dense_emb.0.iter().map(|&x| x as usize).collect::<Vec<_>>() ),
-            dense_emb.1.to_vec(),
-        )?;
+        // high_res_feature_s0
+        // name: high_res_feature_s0
+        // tensor: float32[1,32,256,256]
 
-        Ok((sparse_array, dense_array))
-    }
+        // high_res_feature_s1
+        // name: high_res_feature_s1
+        // tensor: float32[1,64,128,128]
 
-    pub fn decode_mask(
-        &mut self,
-        image_emb: &Array<f32, IxDyn>,
-        sparse_emb: &Array<f32, IxDyn>,
-        dense_emb: &Array<f32, IxDyn>,
-    ) -> OrtResult<Array<f32, IxDyn>> {
-        let mut inputs = OrtInputs::new();
-        inputs.insert("image_embeddings".to_string(), Self::array_to_value(image_emb)?);
-        inputs.insert("sparse_embeddings".to_string(), Self::array_to_value(sparse_emb)?);
-        inputs.insert("dense_embeddings".to_string(), Self::array_to_value(dense_emb)?);
+        // Output
 
-        Self::run_session(&mut self.mask_decoder_session, inputs)
+        // masks
+        // name: masks
+        // tensor: float32[1,1,256,256]
+
+        // iou_pred
+        // name: iou_pred
+        // tensor: float32[1,1]
+
+        // sam_tokens_out
+        // name: sam_tokens_out
+        // tensor: float32[1,1,256]
+
+        // object_score_logits
+        // name: object_score_logits
+        // tensor: float32[1,1]
+
+        let input_image_embeddings: ArrayBase<ViewRepr<&f32>, Dim<[usize; 4]>, f32> =
+            image_embeddings.view();
+        let input_sparse_embeddings: ArrayBase<ViewRepr<&f32>, Dim<[usize; 3]>, f32> =
+            sparse_embeddings.view();
+        let input_dense_embeddings: ArrayBase<ViewRepr<&f32>, Dim<[usize; 4]>, f32> =
+            dense_embeddings.view();
+        let input_high_res_feature_s0: ArrayBase<ViewRepr<&f32>, Dim<[usize; 4]>, f32> =
+            high_res_feature_s0.view();
+        let input_high_res_feature_s1: ArrayBase<ViewRepr<&f32>, Dim<[usize; 4]>, f32> =
+            high_res_feature_s1.view();
+
+        let outputs: &SessionOutputs = &self.mask_decoder_session.run(ort::inputs![
+            "image_embeddings" => TensorRef::from_array_view(input_image_embeddings)?,
+            "sparse_prompt_embeddings" => TensorRef::from_array_view(input_sparse_embeddings)?,
+            "dense_prompt_embeddings" => TensorRef::from_array_view(input_dense_embeddings)?,
+            "high_res_feature_s0" => TensorRef::from_array_view(input_high_res_feature_s0)?,
+            "high_res_feature_s1" => TensorRef::from_array_view(input_high_res_feature_s1)?])?;
+
+        let mask_value: ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>, f32> =
+            Self::_tensor_to_array4::<f32>(outputs, "masks");
+
+        let mask = Mask::new(mask_value);
+        Ok(mask)
     }
 }
 
 impl ImageSegmenter for Sam2Segmenter {
+    type SegmentationInputs = SAM2Inputs;
+    type SegmentationContext = SAM2InferenceContext;
+
     fn segment(
         &mut self,
-        image: Image,
-        points: &[Point],
-    ) -> Result<SegmentedImage, SegmentationErrors> {
-        let image_emb = self
-            .image_embedding
-            .clone()
-            .ok_or(SegmentationErrors::NotPrepared)?;
+        request: Self::SegmentationInputs,
+    ) -> Result<(ApplicationMask, Self::SegmentationContext), SegmentationErrors> {
+        let static_context = request.static_context;
+        let inference_context = request.inference_context;
 
-        let orig_h = image.image_size().height() as f32;
-        let orig_w = image.image_size().width() as f32;
-        let (sparse_emb, dense_emb) = self
-            .encode_prompts(points, orig_h, orig_w)
-            .map_err(|e| SegmentationErrors::InferenceError(format!("Prompt encoding failed: {}", e)))?;
+        let (image_emb, s0, s1) = static_context.get_all_context_refs();
+        let mask = inference_context.get_all_context_refs();
+
+        let mut coords: Vec<(f32, f32)> = Vec::new();
+        let mut labels: Vec<i64> = Vec::new();
+        let mut mask_value: Option<ArrayView4<f32>> = None;
+
+        if let Some(points) = request.points {
+            for point in points {
+                let point_x = point.coordinate().x() as f32;
+                let point_y = point.coordinate().y() as f32;
+                let label = point.label() as i64;
+                coords.push((point_x, point_y));
+                labels.push(label);
+            }
+        }
+
+        if let Some(mask) = mask {
+            mask_value = Some(mask.view())
+        }
+
+        let (sparse_emb, dense_emb) =
+            self._encode_prompt(coords, labels, mask_value)
+                .map_err(|e| {
+                    SegmentationErrors::InferenceError(format!("Prompt encoding failed: {}", e))
+                })?;
+
         let mask = self
-            .decode_mask(image_emb.as_ref(), &sparse_emb, &dense_emb)
-            .map_err(|e| SegmentationErrors::InferenceError(format!("Mask decoding failed: {}", e)))?;
+            ._decode_mask(&image_emb, &sparse_emb, &dense_emb, s0, s1)
+            .map_err(|e| {
+                SegmentationErrors::InferenceError(format!("Mask decoding failed: {}", e))
+            })?;
 
-        let mask_2d = mask.slice(s![0, 0, .., ..]).to_owned();
-        Ok(SegmentedImage::with_mask(image, mask_2d))
+        let mask_2d = mask.view().slice(s![0, 0, .., ..]).to_owned();
+        let context = SAM2InferenceContext::new(Some(mask));
+        Ok((ApplicationMask::new(mask_2d), context))
     }
 }
 
 impl ImageSegmenterPreparing for Sam2Segmenter {
-    fn prepare(&mut self, image: Image) -> Result<(), SegmentationErrors> {
+    type SegmentationContext = SAM2StaticContext;
+
+    fn prepare(&mut self, image: Image) -> Result<Self::SegmentationContext, SegmentationErrors> {
         let img_data = image.image_data().image();
         let img = image::load_from_memory(img_data).map_err(|e| {
             SegmentationErrors::ImageLoadError(format!("Failed to load image: {}", e))
         })?;
 
-        let embedding = self
-            .encode_image(&img)
-            .map_err(|e| SegmentationErrors::InferenceError(format!("Image encoding failed: {}", e)))?;
+        let (embedding, s0, s1) = self._encode_image(&img).map_err(|e| {
+            SegmentationErrors::InferenceError(format!("Image encoding failed: {}", e))
+        })?;
 
-        self.image_embedding = Some(Arc::new(embedding));
-        Ok(())
+        let context = SAM2StaticContext::new(embedding, s0, s1);
+        Ok(context)
     }
 }
--- src/infrastructure/segmenter/sam2_data.rs
+++ src/infrastructure/segmenter/sam2_data.rs
@@ -1,50 +1,146 @@
-use std::rc::Rc;
-
 use crate::domain::value_object::point::Point;
-use ndarray::{Array3, Array4};
+use ndarray::{Array3, Array4, ArrayBase, ArrayView3, ArrayView4, Dim, OwnedRepr};
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
+pub struct ImageEmbeddings {
+    value: Array4<f32>,
+}
+
+impl ImageEmbeddings {
+    pub fn new(value: Array4<f32>) -> Self {
+        Self { value }
+    }
+
+    pub fn view(&self) -> ArrayView4<f32> {
+        self.value.view()
+    }
+}
+
+#[derive(Debug)]
+pub struct HighResFeatureS0 {
+    value: Array4<f32>,
+}
+
+impl HighResFeatureS0 {
+    pub fn new(value: Array4<f32>) -> Self {
+        Self { value }
+    }
+
+    pub fn view(&self) -> ArrayView4<f32> {
+        self.value.view()
+    }
+}
+
+#[derive(Debug)]
+pub struct HighResFeatureS1 {
+    value: Array4<f32>,
+}
+
+impl HighResFeatureS1 {
+    pub fn new(value: Array4<f32>) -> Self {
+        Self { value }
+    }
+
+    pub fn view(&self) -> ArrayView4<f32> {
+        self.value.view()
+    }
+}
+
+#[derive(Debug)]
 pub struct SAM2StaticContext {
-    image_embeddings: Rc<Array4<f32>>,
-    high_res_feature_s0: Rc<Array4<f32>>,
-    high_res_feature_s1: Rc<Array4<f32>>,
+    image_embeddings: ImageEmbeddings,
+    high_res_feature_s0: HighResFeatureS0,
+    high_res_feature_s1: HighResFeatureS1,
 }
 
 impl SAM2StaticContext {
     pub fn new(
-        image_embeddings: Array4<f32>,
-        high_res_feature_s0: Array4<f32>,
-        high_res_feature_s1: Array4<f32>,
+        image_embeddings: ImageEmbeddings,
+        high_res_feature_s0: HighResFeatureS0,
+        high_res_feature_s1: HighResFeatureS1,
     ) -> Self {
         Self {
-            image_embeddings: image_embeddings.into(),
-            high_res_feature_s0: high_res_feature_s0.into(),
-            high_res_feature_s1: high_res_feature_s1.into(),
+            image_embeddings: image_embeddings,
+            high_res_feature_s0: high_res_feature_s0,
+            high_res_feature_s1: high_res_feature_s1,
         }
+    }
+
+    pub fn get_all_context_refs(&self) -> (&ImageEmbeddings, &HighResFeatureS0, &HighResFeatureS1) {
+        (
+            &self.image_embeddings,
+            &self.high_res_feature_s0,
+            &self.high_res_feature_s1,
+        )
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
+pub struct SparseEmbeddings {
+    value: Array3<f32>,
+}
+
+impl SparseEmbeddings {
+    pub fn new(value: Array3<f32>) -> Self {
+        Self { value }
+    }
+
+    pub fn view(&self) -> ArrayView3<f32> {
+        self.value.view()
+    }
+}
+
+#[derive(Debug)]
+pub struct DenseEmbeddings {
+    value: Array4<f32>,
+}
+
+impl DenseEmbeddings {
+    pub fn new(value: Array4<f32>) -> Self {
+        Self { value }
+    }
+
+    pub fn view(&self) -> ArrayView4<f32> {
+        self.value.view()
+    }
+}
+
+#[derive(Debug)]
+pub struct Mask {
+    value: Array4<f32>,
+}
+
+impl Mask {
+    pub fn new(value: Array4<f32>) -> Self {
+        Self { value }
+    }
+
+    pub fn into_mask(self) -> ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>> {
+        self.value
+    }
+
+    pub fn view(&self) -> ArrayView4<f32> {
+        self.value.view()
+    }
+}
+
+#[derive(Debug)]
 pub struct SAM2InferenceContext {
-    sparse_embeddings: Rc<Array3<f32>>,
-    dense_embeddings: Rc<Array4<f32>>,
-    masks: Rc<Array4<f32>>,
+    masks: Option<Mask>,
 }
 
 impl SAM2InferenceContext {
-    pub fn new(
-        sparse_embeddings: Array3<f32>,
-        dense_embeddings: Array4<f32>,
-        masks: Array4<f32>,
-    ) -> Self {
-        Self {
-            sparse_embeddings: sparse_embeddings.into(),
-            dense_embeddings: dense_embeddings.into(),
-            masks: masks.into(),
-        }
+    pub fn new(masks: Option<Mask>) -> Self {
+        Self { masks }
+    }
+
+    pub fn get_all_context_refs(&self) -> &Option<Mask> {
+        &self.masks
     }
 }
 
 pub struct SAM2Inputs {
     pub points: Option<Vec<Point>>,
+    pub static_context: SAM2StaticContext,
+    pub inference_context: SAM2InferenceContext,
 }
