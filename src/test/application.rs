a035e7c410c81c7ef6645f46145fdc33b6b2146f
ChickRen <chiaking207@icloud.com>
2026-05-31 09:39:23 +0900

    fix: format

--- src/test/application.rs
+++ src/test/application.rs
@@ -1,7 +1,7 @@
 pub mod errors;
 pub mod get_image_usecase;
 pub mod interface;
+pub mod point_history;
 pub mod segment_usecase;
 pub mod types;
 pub mod upload_usecase;
-pub mod point_history;
--- src/test/domain/value_object.rs
+++ src/test/domain/value_object.rs
@@ -1,7 +1,7 @@
+pub mod coordinate;
 pub mod image_data;
 pub mod image_id;
 pub mod image_kind;
 pub mod image_size;
-pub mod session_id;
 pub mod point;
-pub mod coordinate;
+pub mod session_id;
