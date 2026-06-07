c00f238abac8da5a97278e8f960d638b50a59253
ChickRen <chiaking207@icloud.com>
2026-06-07 18:02:49 +0900

    feat: implement new constructor for SegmentInput

--- src/application/usecase/segment_usecase/segment_input.rs
+++ src/application/usecase/segment_usecase/segment_input.rs
@@ -12,4 +12,8 @@ impl SegmentInput {
     pub fn into_parts(self) -> (SessionId, ImageId, Vec<Point>) {
         (self.session_id, self.image_id, self.points)
     }
+
+    pub fn new(session_id: SessionId, image_id: ImageId, points: Vec<Point>) -> Self {
+        Self { session_id, image_id, points }
+    }
 }
