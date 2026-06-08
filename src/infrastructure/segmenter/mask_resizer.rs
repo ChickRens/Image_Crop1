b42f61de11243febc0fa8821328e2982a87447a4
ChickRen <chiaking207@icloud.com>
2026-06-08 17:38:44 +0900

    fix: MaskResizer require Array4

--- src/infrastructure/segmenter/mask_resizer.rs
+++ src/infrastructure/segmenter/mask_resizer.rs
@@ -1,12 +1,12 @@
-use ndarray::Array2;
+use ndarray::Array4;
 
 pub struct SAM2MaskResizer;
 
 impl SAM2MaskResizer {
-    pub fn resize_mask(mask: &Array2<f32>, target_h: usize, target_w: usize) -> Array2<f32> {
-        let (src_h, src_w) = mask.dim();
+    pub fn resize_mask(mask: &Array4<f32>, target_h: usize, target_w: usize) -> Array4<f32> {
+        let (batch, channel, src_h, src_w) = mask.dim();
 
-        let mut output = Array2::<f32>::zeros((target_h, target_w));
+        let mut output = Array4::<f32>::zeros((batch, channel ,target_h, target_w));
 
         for y in 0..target_h {
             let src_y = y * src_h / target_h;
@@ -14,7 +14,7 @@ impl SAM2MaskResizer {
             for x in 0..target_w {
                 let src_x = x * src_w / target_w;
 
-                output[[y, x]] = mask[[src_y, src_x]];
+                output[[0, 0, y, x]] = mask[[0, 0, src_y, src_x]];
             }
         }
         output
