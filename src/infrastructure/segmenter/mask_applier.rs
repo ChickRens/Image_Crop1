0ab5aad1287579bdc54956e2280e0b643700a73c
ChickRen <chiaking207@icloud.com>
2026-06-17 21:44:09 +0900

    feat: update SAM2MaskApplier to accept Array4 mask and optimize output generation

--- src/infrastructure/segmenter/mask_applier.rs
+++ src/infrastructure/segmenter/mask_applier.rs
@@ -1,38 +1,33 @@
 use ndarray::prelude::{ArrayBase, Dim};
-use ndarray::{ViewRepr, s};
+use ndarray::{Array4, ViewRepr, s};
 
 use crate::domain::entity::image::Image;
-use crate::infrastructure::segmenter::sam2_data::Mask;
 
 pub struct SAM2MaskApplier;
 
 impl SAM2MaskApplier {
-    pub fn apply(original: &Image, mask: &Mask) -> Vec<u8> {
+    pub fn apply(original: &Image, mask: Array4<f32>) -> Vec<u8> {
         let mask_4d: ArrayBase<ViewRepr<&f32>, Dim<[usize; 4]>, f32> = mask.view();
         let mask_2d: ArrayBase<ViewRepr<&f32>, Dim<[usize; 2]>, f32> =
             mask_4d.slice(s![0, 0, .., ..]);
 
         let original_data = original.image_data().image();
-        let mut output: Vec<u8> = Vec::with_capacity(original_data.len());
-
         let height = original.image_size().height() as usize;
         let width = original.image_size().width() as usize;
 
+        let mut output: Vec<u8> = vec![0u8; width * height * 4];
+
         for y in 0..height {
             for x in 0..width {
-                let index = (y * width + x) * 4;
+                let rgba_index = (y * width + x) * 4;
+                let rgb_index = (y * width + x) * 3;
 
                 let mask_value = &mask_2d[[y, x]];
 
-                if *mask_value < 0.7 as f32 {
-                    output[index + 3] = 0;
-                    continue;
-                }
-
-                output[index] = original_data[index];
-                output[index + 1] = original_data[index + 1];
-                output[index + 2] = original_data[index + 2];
-                output[index + 3] = 1
+                output[rgba_index] = original_data[rgb_index];
+                output[rgba_index + 1] = original_data[rgb_index + 1];
+                output[rgba_index + 2] = original_data[rgb_index + 2];
+                output[rgba_index + 3] = (*mask_value * 255.0).clamp(0.0, 255.0) as u8;
             }
         }
 
