5ac63730351d811727500961278cd2473c3daff1
ChickRen <chiaking207@icloud.com>
2026-06-29 21:07:10 +0900

    refactor: Restructure ImageSegmenter trait to separate context preparation methods

--- src/application/interface/image_segmenter.rs
+++ src/application/interface/image_segmenter.rs
@@ -2,24 +2,31 @@ use crate::application::errors::segmentation_error::SegmentationErrors;
 use crate::application::types::editing_session::CommonEditingSession;
 use crate::application::types::segmented_image::SegmentedImage;
 use crate::domain::entity::image::Image;
+use crate::domain::value_object::point::Point;
 
 pub trait ImageSegmenter {
     type StaticContext;
     type InferenceContext;
 
+    fn prepare_inference_context(
+        &mut self,
+        image: &Image,
+    ) -> Result<Self::InferenceContext, SegmentationErrors>;
+    fn prepare_static_context(
+        &mut self,
+        image: &Image,
+    ) -> Result<Self::StaticContext, SegmentationErrors>;
+    fn rebuild(
+        &mut self,
+        original_image: &Image,
+        static_context: &Self::StaticContext,
+        input_points: &[Point],
+    ) -> Result<(Self::InferenceContext, SegmentedImage), SegmentationErrors>;
     fn segment(
         &mut self,
         original_image: &Image,
-        editing_session: &mut CommonEditingSession<Self::StaticContext, Self::InferenceContext>,
-    ) -> Result<SegmentedImage, SegmentationErrors>;
-}
-
-pub trait ImageSegmenterPreparing {
-    type StaticContext;
-    type InferenceContext;
-
-    fn prepare(
-        &mut self,
-        image: &Image,
-    ) -> Result<(Self::StaticContext, Self::InferenceContext), SegmentationErrors>;
+        static_context: &Self::StaticContext,
+        inference_context: &Self::InferenceContext,
+        input_points: &[Point],
+    ) -> Result<(Self::InferenceContext, SegmentedImage), SegmentationErrors>;
 }
