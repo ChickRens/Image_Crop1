c4f1c7dd0c3d2019786fe50ed51bc1e8825afab1
ChickRen <chiaking207@icloud.com>
2026-06-29 21:13:16 +0900

    refactor: Update SegmentInput to use a single Point instead of a Vec<Point>

--- src/application/usecase/segment_usecase/segment_input.rs
+++ src/application/usecase/segment_usecase/segment_input.rs
@@ -5,19 +5,19 @@ use crate::domain::value_object::session_id::SessionId;
 pub struct SegmentInput {
     session_id: SessionId,
     image_id: ImageId,
-    points: Vec<Point>,
+    point: Point,
 }
 
 impl SegmentInput {
-    pub fn into_parts(self) -> (SessionId, ImageId, Vec<Point>) {
-        (self.session_id, self.image_id, self.points)
+    pub fn into_parts(self) -> (SessionId, ImageId, Point) {
+        (self.session_id, self.image_id, self.point)
     }
 
-    pub fn new(session_id: SessionId, image_id: ImageId, points: Vec<Point>) -> Self {
+    pub fn new(session_id: SessionId, image_id: ImageId, point: Point) -> Self {
         Self {
             session_id,
             image_id,
-            points,
+            point,
         }
     }
 }
