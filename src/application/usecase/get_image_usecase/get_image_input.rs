4bca16ee0a8c7c446174d51946ca1bedd5d7c3c4
ChickRen <chiaking207@icloud.com>
2026-08-05 20:51:32 +0900

    refactor: GetImageUseCase do not use Session

--- src/application/usecase/get_image_usecase/get_image_input.rs
+++ src/application/usecase/get_image_usecase/get_image_input.rs
@@ -1,21 +1,17 @@
-use crate::domain::value_object::{image_id::image_id::ImageId, image_kind::ImageKind, session_id::session_id::SessionId};
+use crate::domain::value_object::image_id::image_id::ImageId;
 
 pub struct GetImageInput {
-    session_id: SessionId,
     image_id: ImageId,
-    image_kind: ImageKind,
 }
 
 impl GetImageInput {
-    pub fn new(session_id: SessionId, image_id: ImageId, image_kind: ImageKind) -> Self {
+    pub fn new(image_id: ImageId) -> Self {
         Self {
-            session_id,
             image_id,
-            image_kind,
         }
     }
 
-    pub fn into_session_id_image_id_image_kind(self) -> (SessionId, ImageId, ImageKind) {
-        (self.session_id, self.image_id, self.image_kind)
+    pub fn image_id(&self) -> ImageId {
+        self.image_id
     }
 }
--- src/application/usecase/get_image_usecase/usecase.rs
+++ src/application/usecase/get_image_usecase/usecase.rs
@@ -1,58 +1,29 @@
-use crate::application::errors::application_errors::ApplicationErrors;
-use crate::application::errors::repository_errors::RepositoryErrors;
-use crate::application::errors::validation::session_errors::SessionErrors;
-use crate::application::errors::validation_errors::ValidationErrors;
-use crate::application::usecase::get_image_usecase::get_image_input::GetImageInput;
-use crate::application::usecase::get_image_usecase::get_image_output::GetImageOutput;
-use crate::domain::entity::image::Image;
-use crate::domain::entity::session::Session;
-use crate::domain::repository::image_repository::ImageRepository;
-use crate::domain::repository::session_repository::SessionRepository;
+use crate::{application::usecase::get_image_usecase::{error::GetImageUseCaseError, get_image_input::GetImageInput, get_image_output::GetImageOutput}, domain::repository::image_repository::repository::ImageRepository};
 
-pub struct GetImageUseCase<SR, IR>
+pub struct GetImageUseCase<IR>
 where
-    SR: SessionRepository,
     IR: ImageRepository,
 {
-    session_repo: SR,
     image_repo: IR,
 }
 
-impl<SR, IR> GetImageUseCase<SR, IR>
+impl<IR> GetImageUseCase<IR>
 where
-    SR: SessionRepository,
     IR: ImageRepository,
 {
-    pub fn new(session_repository: SR, image_repository: IR) -> Self {
+    pub fn new(image_repository: IR) -> Self {
         Self {
-            session_repo: session_repository,
             image_repo: image_repository,
         }
     }
 
-    pub fn execute(&self, input: GetImageInput) -> Result<GetImageOutput, ApplicationErrors> {
-        let (session_id, image_id, image_kind) = input.into_session_id_image_id_image_kind();
+    pub fn execute(&self, input: GetImageInput) -> Result<GetImageOutput, GetImageUseCaseError> {
+        let image_id = input.image_id();
 
-        let session: Session =
-            self.session_repo
-                .get(&session_id)
-                .ok_or(ApplicationErrors::RepositoryError(
-                    RepositoryErrors::SessionNotFound,
-                ))?;
-
-        let is_valid_image_id: bool = session.has_image_id(&image_id);
-        if !is_valid_image_id {
-            return Err(ApplicationErrors::ValidationError(
-                ValidationErrors::Session(SessionErrors::ImageNotOwned),
-            ));
-        }
-
-        let image: Image = self.image_repo.get(&image_id, image_kind).ok_or(
-            ApplicationErrors::RepositoryError(RepositoryErrors::ImageNotFound),
-        )?;
+        let image = self.image_repo.get(&image_id)?;
 
         let (data, _id, _size) = image.into_data();
-        let output: GetImageOutput = GetImageOutput::new(data.into_image());
+        let output= GetImageOutput::new(data.into_image());
 
         Ok(output)
     }
