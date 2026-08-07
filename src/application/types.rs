01e2edf751281d0d283201031553a1bd919474d0
ChickRen <chiaking207@icloud.com>
2026-08-07 13:12:44 +0900

    feat: create RenderedImage

--- src/application/types.rs
+++ src/application/types.rs
@@ -2,3 +2,4 @@ pub mod editing_session;
 pub mod loaded_image;
 pub mod point_history;
 pub mod segmented_image;
+pub mod rendered_image;
---
+++ src/application/types/rendered_image.rs
@@ -0,0 +1,21 @@
+use crate::domain::value_object::{image_data::ImageData, image_id::image_id::ImageId, image_size::image_size::ImageSize};
+
+pub struct RenderedImage {
+    data: ImageData,
+    id: ImageId,
+    size: ImageSize,
+}
+
+impl RenderedImage {
+    pub fn new(data: ImageData, id: ImageId, size: ImageSize) -> Self {
+        Self { data, id, size }
+    }
+
+    pub fn image_id(&self) -> ImageId {
+        self.id
+    }
+
+    pub fn into_data(self) -> (ImageData, ImageId, ImageSize) {
+        (self.data, self.id, self.size)
+    }
+}
