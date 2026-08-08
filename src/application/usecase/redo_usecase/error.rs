284b7b6bd3390fe98a2c77dd16e724181f169330
ChickRen <chiaking207@icloud.com>
2026-08-08 22:15:26 +0900

    refactor: RedoUseCase has RenderedImageCache

--- src/application/usecase/redo_usecase/error.rs
+++ src/application/usecase/redo_usecase/error.rs
@@ -1,10 +1,11 @@
-use crate::{application::interface::{editing_session_repository::error::EditingSessionRepositoryError, image_segmenter::error::SegmenterError}, domain::repository::{image_repository::error::ImageRepositoryError, session_repository::error::SessionRepositoryError}, parent_error};
+use crate::{application::{interface::{editing_session_repository::error::EditingSessionRepositoryError, image_segmenter::error::SegmenterError}, types::editing_session::error::EditingSessionError}, domain::repository::{original_image_repository::error::OriginalImageRepositoryError, session_repository::error::SessionRepositoryError}, parent_error};
 
 parent_error!(
     pub enum RedoUseCaseError {
-        ImageRepository(ImageRepositoryError),
+        ImageRepository(OriginalImageRepositoryError),
         SessionRepository(SessionRepositoryError),
         EditingSessionRepository(EditingSessionRepositoryError),
         Segmenter(SegmenterError),
+        EditingSession(EditingSessionError),
     }
 );
--- src/application/usecase/redo_usecase/usecase.rs
+++ src/application/usecase/redo_usecase/usecase.rs
@@ -1,42 +1,47 @@
-use crate::{application::{interface::{editing_session_repository::repository::EditingSessionRepository, image_segmenter::segmenter::ImageSegmenter}, types::editing_session::EditingSession, usecase::redo_usecase::{error::RedoUseCaseError, redo_input::RedoInput, redo_output::RedoOutput}}, domain::{entity::image::image::Image, repository::{image_repository::repository::ImageRepository, session_repository::repository::SessionRepository}, value_object::image_id::image_id::ImageId}};
+use crate::{application::{interface::{editing_session_repository::repository::EditingSessionRepository, image_segmenter::segmenter::ImageSegmenter, rendered_image_cache::cache::RenderedImageCache}, types::{editing_session::session::EditingSession, rendered_image::RenderedImage}, usecase::redo_usecase::{error::RedoUseCaseError, redo_input::RedoInput, redo_output::RedoOutput}}, domain::{repository::{original_image_repository::repository::OriginalImageRepository, session_repository::repository::SessionRepository}, value_object::image_id::image_id::ImageId}};
 
-pub struct RedoUseCase<SR, IR, IS, ESR>
+pub struct RedoUseCase<SR, IR, IS, ESR, IC>
 where
     SR: SessionRepository,
     IS: ImageSegmenter,
-    IR: ImageRepository,
+    IR: OriginalImageRepository,
     ESR: EditingSessionRepository<
             StaticContext = <IS as ImageSegmenter>::StaticContext,
             InferenceContext = <IS as ImageSegmenter>::InferenceContext,
         >,
+    IC: RenderedImageCache
 {
     session_repo: SR,
     image_repo: IR,
     segmenter: IS,
     editing_session_repo: ESR,
+    image_cache: IC
 }
 
-impl<SR, IR, IS, ESR> RedoUseCase<SR, IR, IS, ESR>
+impl<SR, IR, IS, ESR, IC> RedoUseCase<SR, IR, IS, ESR, IC>
 where
     SR: SessionRepository,
-    IR: ImageRepository,
+    IR: OriginalImageRepository,
     IS: ImageSegmenter,
     ESR: EditingSessionRepository<
             StaticContext = <IS as ImageSegmenter>::StaticContext,
             InferenceContext = <IS as ImageSegmenter>::InferenceContext,
         >,
+    IC: RenderedImageCache
 {
     pub fn new(
         session_repository: SR,
         image_repository: IR,
         image_segmenter: IS,
         editing_session_repository: ESR,
+        rendered_image_cache: IC
     ) -> Self {
         Self {
             session_repo: session_repository,
             image_repo: image_repository,
             segmenter: image_segmenter,
             editing_session_repo: editing_session_repository,
+            image_cache: rendered_image_cache,
         }
     }
 
@@ -44,29 +49,27 @@ where
         let session_id = redo_input.session_id();
         let session = self.session_repo.get(&session_id)?;
 
-        let image_id = session.image_id();
-        let image = self.image_repo.get(image_id)?;
+        let original_image_id = session.image_id();
+        let original_image = self.image_repo.get(original_image_id)?;
 
         let mut editing_session = self.editing_session_repo.get(&session_id)?;
 
-        editing_session.redo();
+        editing_session.redo()?;
         let points = editing_session.points();
         let static_context = editing_session.static_context();
         let inference_context = editing_session.inference_context();
 
-        let (new_context, segmented_image) =
+        let (_new_context, segmented_image) =
             self.segmenter
-                .segment(&image, static_context, inference_context, points)?;
-        editing_session.set_inference_context(new_context);
+                .segment(&original_image, static_context, inference_context, points)?;
 
         let (segmented_image_data, size) = segmented_image.into_image_and_size();
         let segmented_image_id = ImageId::new();
 
         self.editing_session_repo.save(&session_id, editing_session);
 
-        self.image_repo.save(
-            Image::new(segmented_image_data, segmented_image_id, size)
-        );
+        self.image_cache.save(RenderedImage::new(
+            segmented_image_data, segmented_image_id, size));
 
         let output = RedoOutput::new(segmented_image_id);
         Ok(output)
