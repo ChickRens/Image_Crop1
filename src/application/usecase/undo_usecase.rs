ad98aaa8070756a5ff048b8dd7754ceda9abcd2f
ChickRen <chiaking207@icloud.com>
2026-08-08 22:21:45 +0900

    feat: UndoUseCase returns UndoUseCaseError

--- src/application/usecase/undo_usecase.rs
+++ src/application/usecase/undo_usecase.rs
@@ -1,3 +1,4 @@
 pub mod undo_input;
 pub mod undo_output;
 pub mod usecase;
+pub mod error;
--- src/application/usecase/undo_usecase/usecase.rs
+++ src/application/usecase/undo_usecase/usecase.rs
@@ -1,96 +1,77 @@
-use crate::{
-    application::{
-        errors::{application_errors::ApplicationErrors, repository_errors::RepositoryErrors},
-        interface::{
-            editing_session_repository::EditingSessionRepository, image_segmenter::ImageSegmenter,
-        },
-        types::editing_session::EditingSession,
-        usecase::undo_usecase::{undo_input::UndoInput, undo_output::UndoOutput},
-    },
-    domain::{
-        entity::image::Image,
-        repository::{image_repository::ImageRepository, session_repository::SessionRepository},
-        value_object::{image_id::ImageId, image_kind::ImageKind},
-    },
-};
+use crate::{application::{interface::{editing_session_repository::repository::EditingSessionRepository, image_segmenter::segmenter::ImageSegmenter, rendered_image_cache::cache::RenderedImageCache}, types::{editing_session::session::EditingSession, rendered_image::RenderedImage}, usecase::undo_usecase::{error::UndoUseCaseError, undo_input::UndoInput, undo_output::UndoOutput}}, domain::{repository::{original_image_repository::repository::OriginalImageRepository, session_repository::repository::SessionRepository}, value_object::image_id::image_id::ImageId}};
 
-pub struct UndoUseCase<SR, IR, IS, ESR>
+pub struct UndoUseCase<SR, IR, IS, ESR, IC>
 where
     SR: SessionRepository,
     IS: ImageSegmenter,
-    IR: ImageRepository,
+    IR: OriginalImageRepository,
     ESR: EditingSessionRepository<
             StaticContext = <IS as ImageSegmenter>::StaticContext,
             InferenceContext = <IS as ImageSegmenter>::InferenceContext,
         >,
+    IC: RenderedImageCache,
 {
     session_repo: SR,
     image_repo: IR,
     segmenter: IS,
     editing_session_repo: ESR,
+    image_cache: IC,
 }
 
-impl<SR, IR, IS, ESR> UndoUseCase<SR, IR, IS, ESR>
+impl<SR, IR, IS, ESR, IC> UndoUseCase<SR, IR, IS, ESR, IC>
 where
     SR: SessionRepository,
-    IR: ImageRepository,
+    IR: OriginalImageRepository,
     IS: ImageSegmenter,
     ESR: EditingSessionRepository<
             StaticContext = <IS as ImageSegmenter>::StaticContext,
             InferenceContext = <IS as ImageSegmenter>::InferenceContext,
         >,
+    IC: RenderedImageCache,
 {
     pub fn new(
         session_repository: SR,
         image_repository: IR,
         image_segmenter: IS,
         editing_session_repository: ESR,
+        rendered_image_cache: IC,
     ) -> Self {
         Self {
             session_repo: session_repository,
             image_repo: image_repository,
             segmenter: image_segmenter,
             editing_session_repo: editing_session_repository,
+            image_cache: rendered_image_cache,
         }
     }
 
-    pub fn execute(&self, undo_input: UndoInput) -> Result<UndoOutput, ApplicationErrors> {
+    pub fn execute(&self, undo_input: UndoInput) -> Result<UndoOutput, UndoUseCaseError> {
         let session_id = undo_input.session_id();
-        let session =
-            self.session_repo
-                .get(&session_id)
-                .ok_or(ApplicationErrors::RepositoryError(
-                    RepositoryErrors::SessionNotFound,
-                ))?;
+        let session = self.session_repo.get(&session_id)?;
 
-        let image_id = session.image_id();
-        let image = self.image_repo.get(image_id, ImageKind::Original).ok_or(
-            ApplicationErrors::RepositoryError(RepositoryErrors::ImageNotFound),
-        )?;
+        let original_image_id = session.image_id();
+        let original_image = self.image_repo.get(original_image_id)?;
 
-        let mut editing_session = self.editing_session_repo.get(&session_id).ok_or(
-            ApplicationErrors::RepositoryError(RepositoryErrors::SessionNotFound),
-        )?;
+        let mut editing_session = self.editing_session_repo.get(&session_id)?;
 
-        editing_session.undo();
+        editing_session.undo()?;
         let points = editing_session.points();
         let static_context = editing_session.static_context();
+        let inference_context = editing_session.inference_context();
 
-        let (new_context, segmented_image) =
-            self.segmenter.rebuild(&image, static_context, points)?;
-        editing_session.set_inference_context(new_context);
+        let (new_context, segmented_image) = self.segmenter.segment(&original_image, static_context, inference_context, points)?;
+        editing_session.update_inference_context(new_context);
 
         let (segmented_image_data, size) = segmented_image.into_image_and_size();
         let segmented_image_id = ImageId::new();
 
         self.editing_session_repo.save(&session_id, editing_session);
 
-        self.image_repo.save(
-            Image::new(segmented_image_data, segmented_image_id, size),
-            ImageKind::Segmented,
+        self.image_cache.save(
+            RenderedImage::new(segmented_image_data, segmented_image_id, size),
         );
 
-        let output = UndoOutput::new(session_id, segmented_image_id);
+        let output = UndoOutput::new(segmented_image_id);
         Ok(output)
     }
 }
