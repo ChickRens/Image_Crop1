8c640ae0c862c2ba4f723a01643d8e928e990ad0
ChickRen <chiaking207@icloud.com>
2026-08-09 20:46:30 +0900

    feat: create ApplicationError

--- src/application.rs
+++ src/application.rs
@@ -1,3 +1,4 @@
 pub mod interface;
 pub mod types;
 pub mod usecase;
+pub mod error;
---
+++ src/application/error.rs
@@ -0,0 +1,11 @@
+use crate::{application::usecase::{get_image_usecase::error::GetImageUseCaseError, redo_usecase::error::RedoUseCaseError, segment_usecase::error::SegmentUseCaseError, undo_usecase::error::UndoUseCaseError, upload_usecase::error::UploadUseCaseError}, parent_error};
+
+parent_error!(
+    pub enum ApplicationError {
+        GetImageUseCase(GetImageUseCaseError),
+        RedoUseCase(RedoUseCaseError),
+        SegmentUseCase(SegmentUseCaseError),
+        UndoUseCase(UndoUseCaseError),
+        UploadUseCase(UploadUseCaseError),
+    }
+);
