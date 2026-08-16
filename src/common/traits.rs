e9caad8b7efb79f346d80b806060a24fbafcbbd1
ChickRen <chiaking207@icloud.com>
2026-08-16 15:15:13 +0900

    feat: create ErrorType and ErrorTypeProvider

--- src/common/traits.rs
+++ src/common/traits.rs
@@ -5,3 +5,15 @@ pub trait Code {
 pub trait Cause {
     fn cause(&self) -> Option<&str>;
 }
+
+pub enum ErrorType {
+    InvalidInput,
+    Conflict,
+    NotFound,
+    Internal,
+}
+
+pub trait ErrorTypeProvider {
+    fn error_type(&self) -> ErrorType;
+}
+
