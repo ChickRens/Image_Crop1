252875f4a1e4f54c42cf9bd9168c5d7a9e13d523
ChickRen <chiaking207@icloud.com>
2026-08-16 20:56:58 +0900

    feat: create AppError and implement IntoResponse

---
+++ src/presentation/errors/app_error.rs
@@ -0,0 +1,32 @@
+use axum::{Json, http::StatusCode, response::{IntoResponse, Response}};
+
+use crate::{application::error::ApplicationError, common::traits::{Code, ErrorType, ErrorTypeProvider}, parent_error, presentation::errors::presentation_error::PresentationError};
+
+parent_error!(
+    pub enum AppError {
+        ApplicationError(ApplicationError),
+        Presentation(PresentationError),
+    }
+);
+
+#[derive(serde::Serialize)]
+struct ErrorBody {
+    code: String
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let status = match self.error_type() {
+            ErrorType::NotFound => StatusCode::NOT_FOUND,
+            ErrorType::Conflict => StatusCode::CONFLICT,
+            ErrorType::Internal => StatusCode::INTERNAL_SERVER_ERROR,
+            ErrorType::InvalidInput => StatusCode::BAD_REQUEST,
+        };
+
+        let code = self.code();
+
+        let json = Json(ErrorBody {code: code.to_string()});
+
+        (status, json).into_response()
+    }
+}
