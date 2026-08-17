bcbd1134545015bacd762c2c490123fee2d29e43
ChickRen <chiaking207@icloud.com>
2026-08-18 00:19:33 +0900

    feat: add SessionIdError and ImageIdError to PresentationError

--- src/presentation/errors/presentation_error.rs
+++ src/presentation/errors/presentation_error.rs
@@ -1,9 +1,46 @@
-use crate::{common::traits::ErrorType::{Internal, InvalidInput}, leaf_detail_error};
+use crate::{common::traits::{Cause, Code, ErrorType::{self, Internal, InvalidInput}, ErrorTypeProvider}, domain::value_object::{image_id::error::ImageIdError, session_id::error::SessionIdError}};
 
-leaf_detail_error!(
-    pub enum PresentationError {
-        Multipart => ("MULTIPART_ERROR", Internal),
-        UnknownName => ("UNKNOWN_TAG_NAME", InvalidInput),
-        NoName => ("NO_TAG_NAME_ERROR", InvalidInput),
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum PresentationError {
+    Multipart(String),
+    UnknownName(String),
+    SessionId(SessionIdError),
+    ImageId(ImageIdError),
+    NoName,
+}
+
+impl Code for PresentationError {
+    fn code(&self) -> &str {
+        match self {
+            Self::Multipart(_) => "MULTIPART_ERROR",
+            Self::UnknownName(_) => "UNKNOWN_TAG_NAME",
+            Self::ImageId(err) => err.code(),
+            Self::SessionId(err) => err.code(),
+            Self::NoName => "NO_TAG_NAME_ERROR",
+        }
     }
-);
+}
+
+impl Cause for PresentationError {
+    fn cause(&self) -> Option<&str> {
+        match self {
+            Self::Multipart(cause) => Some(cause),
+            Self::UnknownName(cause) => Some(cause),
+            Self::ImageId(err) => err.cause(),
+            Self::SessionId(err) => err.cause(),
+            Self::NoName => None,
+        }
+    }
+}
+
+impl ErrorTypeProvider for PresentationError {
+    fn error_type(&self) -> ErrorType {
+        match self {
+            Self::Multipart(_) => Internal,
+            Self::UnknownName(_) => InvalidInput,
+            Self::ImageId(err) => err.error_type(),
+            Self::SessionId(err) => err.error_type(),
+            Self::NoName => InvalidInput,
+        }
+    }
+}
