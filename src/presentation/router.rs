0a7fc03a1a386a1a92424c10bc27c50d94801e0c
ChickRen <chiaking207@icloud.com>
2026-08-17 21:48:52 +0900

    feat: update fallback service directory for frontend assets

--- src/presentation/router.rs
+++ src/presentation/router.rs
@@ -8,7 +8,7 @@ use crate::{composition::wiring::App, presentation::handler::upload::handler::up
 pub fn route(app: Arc<App>) -> Router {
     Router::new()
     .route("/upload", post(upload))
-    .fallback_service(ServeDir::new("frontend/pages")
+    .fallback_service(ServeDir::new("frontend")
     .append_index_html_on_directories(true)
     ).with_state(app)
 }
