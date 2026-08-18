d1bfded7542e2c8405be919a9652d6cf9399229b
ChickRen <chiaking207@icloud.com>
2026-08-18 17:06:54 +0900

    feat: GetImageOutput has ImageSize

--- src/application/usecase/get_image_usecase/get_image_output.rs
+++ src/application/usecase/get_image_usecase/get_image_output.rs
@@ -1,13 +1,16 @@
+use crate::domain::value_object::image_size::image_size::ImageSize;
+
 pub struct GetImageOutput {
     data: Vec<u8>,
+    size: ImageSize,
 }
 
 impl GetImageOutput {
-    pub fn new(data: Vec<u8>) -> Self {
-        Self { data }
+    pub fn new(data: Vec<u8>, size: ImageSize) -> Self {
+        Self { data , size}
     }
 
-    pub fn into_image_data(self) -> Vec<u8> {
-        self.data
+    pub fn into_image_data(self) -> (Vec<u8>, ImageSize) {
+        (self.data, self.size)
     }
 }
