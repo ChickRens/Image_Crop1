863f2f34d77f8814d7ae16cf81e59ab00e0ec7f0
ChickRen <chiaking207@icloud.com>
2026-08-18 20:26:10 +0900

    feat: add LoadingError and ConvertToPngError to PresentationError

--- src/presentation/errors/presentation_error.rs
+++ src/presentation/errors/presentation_error.rs
@@ -4,8 +4,10 @@ use crate::{common::traits::{Cause, Code, ErrorType::{self, Internal, InvalidInp
 pub enum PresentationError {
     Multipart(String),
     UnknownName(String),
+    ConvertToPng(String),
     SessionId(SessionIdError),
     ImageId(ImageIdError),
+    Loading,
     NoName,
 }
 
@@ -14,8 +16,10 @@ impl Code for PresentationError {
         match self {
             Self::Multipart(_) => "MULTIPART_ERROR",
             Self::UnknownName(_) => "UNKNOWN_TAG_NAME",
+            Self::ConvertToPng(_) => "CONVERT_TO_PNG_ERROR",
             Self::ImageId(err) => err.code(),
             Self::SessionId(err) => err.code(),
+            Self::Loading => "LOADING_ERROR",
             Self::NoName => "NO_TAG_NAME_ERROR",
         }
     }
@@ -26,10 +30,24 @@ impl Cause for PresentationError {
         match self {
             Self::Multipart(cause) => Some(cause),
             Self::UnknownName(cause) => Some(cause),
+            Self::ConvertToPng(cause) => Some(cause),
+            Self::Loading => None,
             Self::ImageId(err) => err.cause(),
             Self::SessionId(err) => err.cause(),
             Self::NoName => None,
         }
+    }
+}
+
+impl From<ImageIdError> for PresentationError {
+    fn from(value: ImageIdError) -> Self {
+        Self::ImageId(value)
+    }
+}
+
+impl From<SessionIdError> for PresentationError {
+    fn from(value: SessionIdError) -> Self {
+        Self::SessionId(value)
     }
 }
 
@@ -38,6 +56,8 @@ impl ErrorTypeProvider for PresentationError {
         match self {
             Self::Multipart(_) => Internal,
             Self::UnknownName(_) => InvalidInput,
+            Self::ConvertToPng(_) => Internal,
+            Self::Loading => Internal,
             Self::ImageId(err) => err.error_type(),
             Self::SessionId(err) => err.error_type(),
             Self::NoName => InvalidInput,
