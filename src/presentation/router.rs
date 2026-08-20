411b8d3d735e46c919e2dc02737dc16107ddbed8
ChickRen <chiaking207@icloud.com>
2026-08-20 13:15:46 +0900

    feat: add route for get_image handler in router

--- src/presentation/router.rs
+++ src/presentation/router.rs
@@ -3,11 +3,13 @@ use std::sync::Arc;
 use axum::{Router, routing::post};
 use tower_http::services::ServeDir;
 
-use crate::{composition::wiring::App, presentation::handler::upload::handler::upload};
+use crate::{composition::wiring::App, presentation::handler::{get_image::handler::get_image, segment::handler::segment, upload::handler::upload}};
 
 pub fn route(app: Arc<App>) -> Router {
     Router::new()
     .route("/upload", post(upload))
+    .route("/get-image", post(get_image))
+    .route("/segment", post(segment))
     .fallback_service(ServeDir::new("frontend")
     .append_index_html_on_directories(true)
     ).with_state(app)
