06f8fc3f376fcf5944b638d55e0a6db3cc12cdf0
ChickRen <chiaking207@icloud.com>
2026-08-20 12:49:28 +0900

    feat: implement segment handler for image segmentation

---
+++ src/presentation/handler/segment/handler.rs
@@ -0,0 +1,30 @@
+use std::sync::Arc;
+
+use axum::{Json, extract::State};
+
+use crate::{application::usecase::segment_usecase::segment_input::SegmentInput, composition::wiring::App, domain::value_object::{coordinate::Coordinate, point::{Point, PointLabel}, session_id::session_id::SessionId}, presentation::{errors::{app_error::AppError, presentation_error::PresentationError}, handler::segment::{request::SegmentRequest, response::SegmentResponse}}};
+
+#[axum::debug_handler]
+pub async fn segment(
+    State(app): State<Arc<App>>,
+    Json(request): Json<SegmentRequest>,
+) -> Result<Json<SegmentResponse>, AppError>{
+    let session_id = request.session_id;
+    let (x, y) = request.point;
+    let is_foreground = request.is_foreground;
+    let session_id = SessionId::from_str(&session_id)
+        .map_err(|err| PresentationError::from(err))?;
+
+    let label = match is_foreground {
+        true => PointLabel::FOREGROUND,
+        false => PointLabel::BACKGROUND,
+    };
+
+    let point = Point::new(Coordinate::new(x, y), label);
+    let input = SegmentInput::new(session_id, point);
+
+    let output = app.segment(input)?;
+
+    let response = SegmentResponse::new(*output.image_id().value());
+    Ok(Json(response))
+}
