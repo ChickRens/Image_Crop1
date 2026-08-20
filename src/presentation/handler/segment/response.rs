5f5f9eab245cee90b526f81946b67b635f37c816
ChickRen <chiaking207@icloud.com>
2026-08-20 13:16:31 +0900

    feat: segment returns SegmentResponse

---
+++ src/presentation/handler/segment/response.rs
@@ -0,0 +1,12 @@
+use uuid::Uuid;
+
+#[derive(serde::Serialize)]
+pub struct SegmentResponse {
+    image_id: Uuid
+}
+
+impl SegmentResponse {
+    pub fn new(image_id: Uuid) -> Self {
+        Self { image_id }
+    }
+}
