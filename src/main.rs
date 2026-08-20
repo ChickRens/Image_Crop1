a8a50499e88bfe0d49c8faba1de8393f7ea99f40
ChickRen <chiaking207@icloud.com>
2026-08-20 21:01:48 +0900

    refactor: uncomment main application logic and add redo route to the router

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,19 @@ mod infrastructure;
 mod presentation;
 mod test;
 
-// use std::sync::Arc;
+use std::sync::Arc;
 
-// use presentation::router;
-// use tokio::net::TcpListener;
+use presentation::router;
+use tokio::net::TcpListener;
 
-// use crate::composition::wiring::App;
+use crate::composition::wiring::App;
 
 #[tokio::main]
 async fn main() {
-    // let app = Arc::new(App::new().unwrap());
-    // let router = router::route(app);
-    // println!("Server running on http://localhost:3000");
+    let app = Arc::new(App::new().unwrap());
+    let router = router::route(app);
+    println!("Server running on http://localhost:3000");
 
-    // let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    // axum::serve(listener, router).await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
