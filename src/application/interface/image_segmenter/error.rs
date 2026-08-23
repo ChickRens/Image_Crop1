0111fd4f66b4bb0ee87bf0edfedf1cef04bfa5aa
ChickRen <chiaking207@icloud.com>
2026-08-23 21:59:21 +0900

    refactor: remove SegmenterError and add SegmenterRuntimeError, SegmenterLoadingError, SegmenterModelError

--- src/application/interface/image_segmenter/error.rs
+++ src/application/interface/image_segmenter/error.rs
@@ -1,9 +1,19 @@
 use crate::{common::traits::ErrorType::Internal, leaf_detail_error};
 
 leaf_detail_error!(
-    pub enum SegmenterError {
+    pub enum SegmenterModelError {
         ModelLoadError => ("MODEL_LOAD_ERROR", Internal),
+    }
+);
+
+leaf_detail_error!(
+    pub enum SegmenterRuntimeError {
         InferenceError => ("INFERENCE_ERROR", Internal),
+    }
+);
+
+leaf_detail_error!(
+    pub enum SegmenterLoadingError {
         PreProcessError => ("PREPROCESS_ERROR", Internal),
     }
 );
