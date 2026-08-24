c8050399f9596e542c9d4359c22e4d36a933764a
ChickRen <chiaking207@icloud.com>
2026-08-24 15:24:12 +0900

    feat: add SegmenterInputImageStorage and error

--- src/application/interface.rs
+++ src/application/interface.rs
@@ -3,3 +3,4 @@ pub mod image_loader;
 pub mod image_segmenter;
 pub mod rendered_image_cache;
 pub mod preview_storage;
+pub mod segmenter_input_image_storage;
---
+++ src/application/interface/segmenter_input_image_storage/error.rs
@@ -0,0 +1,7 @@
+use crate::{common::traits::ErrorType::NotFound, leaf_error};
+
+leaf_error!(
+    pub enum SegmenterInputImageStorageError {
+        ImageNotFound => ("IMAGE_NOT_FOUND", NotFound),
+    }
+);
