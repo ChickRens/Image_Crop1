24324e600dd0d8c817a9d7683f27bccce621049a
ChickRen <chiaking207@icloud.com>
2026-08-24 14:42:10 +0900

    refactor: update OriginalImageRepository to use OriginalImage type

--- src/domain/repository/original_image_repository/repository.rs
+++ src/domain/repository/original_image_repository/repository.rs
@@ -1,8 +1,8 @@
-use crate::domain::entity::image::Image;
+use crate::domain::entity::original_image::OriginalImage;
 use crate::domain::repository::original_image_repository::error::OriginalImageRepositoryError;
 use crate::domain::value_object::image_id::image_id::ImageId;
 
 pub trait OriginalImageRepository {
-    fn save(&self, image: Image);
-    fn get(&self, image_id: &ImageId) -> Result<Image, OriginalImageRepositoryError>;
+    fn save(&self, image: OriginalImage);
+    fn get(&self, image_id: &ImageId) -> Result<OriginalImage, OriginalImageRepositoryError>;
 }
