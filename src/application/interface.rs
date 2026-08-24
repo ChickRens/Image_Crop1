9fdb47dac103f8260a93c55a3b16c9366a6bedd6
ChickRen <chiaking207@icloud.com>
2026-08-24 15:22:11 +0900

    feat: add PreviewStorage and error

--- src/application/interface.rs
+++ src/application/interface.rs
@@ -2,3 +2,4 @@ pub mod editing_session_repository;
 pub mod image_loader;
 pub mod image_segmenter;
 pub mod rendered_image_cache;
+pub mod preview_storage;
---
+++ src/application/interface/preview_storage/storage.rs
@@ -0,0 +1,6 @@
+use crate::{application::{interface::preview_storage::error::PreviewStorageError, types::preview_image::PreviewImage}, domain::value_object::image_id::image_id::ImageId};
+
+pub trait PreviewStorage {
+    fn save(&self, image: PreviewImage);
+    fn get(&self, image_id: ImageId) -> Result<PreviewImage, PreviewStorageError>;
+}
