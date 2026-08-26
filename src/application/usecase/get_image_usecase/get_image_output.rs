7ee0050edd7f65808ba366a00da1abdeeb8b2cd1
ChickRen <chiaking207@icloud.com>
2026-08-26 23:03:11 +0900

    fix: Refactor GetImageOutput to use a reference for image data for improved performance

--- src/application/usecase/get_image_usecase/get_image_output.rs
+++ src/application/usecase/get_image_usecase/get_image_output.rs
@@ -1,16 +1,16 @@
 use crate::domain::value_object::image_size::image_size::ImageSize;
 
-pub struct GetImageOutput {
-    data: Vec<u8>,
+pub struct GetImageOutput<'a> {
+    data: &'a [u8],
     size: ImageSize,
 }
 
-impl GetImageOutput {
-    pub fn new(data: Vec<u8>, size: ImageSize) -> Self {
-        Self { data , size}
+impl<'a> GetImageOutput<'a> {
+    pub fn new(data: &'a [u8], size: ImageSize) -> Self {
+        Self { data: data , size}
     }
 
-    pub fn into_image_data(self) -> (Vec<u8>, ImageSize) {
-        (self.data, self.size)
+    pub fn image_data(&self) -> &'a[u8] {
+        self.data
     }
 }
