13f7f9657b2a31ef58e0fe96535cbaed87fb4134
ChickRen <chiaking207@icloud.com>
2026-08-26 22:01:28 +0900

    fix: Update PreviewImageGenerator and SegmenterInputImageGenerator to accept references for improved performance

--- src/application/interface/preview_image_generator.rs
+++ src/application/interface/preview_image_generator.rs
@@ -1,5 +1,5 @@
 use crate::{application::types::preview_image::PreviewImage, domain::entity::image::Image};
 
 pub trait PreviewImageGenerator {
-    fn generate(&self, image: Image) -> PreviewImage;
+    fn generate(&self, image: &Image) -> PreviewImage;
 }
