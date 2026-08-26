1adf6caa4669abeff69e8ff9b21370dd49e13d28
ChickRen <chiaking207@icloud.com>
2026-08-26 16:59:13 +0900

    feat: refactor UploadUseCase to integrate PreviewImageGenerator and PreviewStorage

--- src/application/usecase/upload_usecase/usecase.rs
+++ src/application/usecase/upload_usecase/usecase.rs
@@ -1,61 +1,54 @@
 use crate::{
     application::{
         interface::{
-            editing_session_repository::repository::EditingSessionRepository,
-            image_loader::loader::ImageLoader, image_segmenter::segmenter::ImageSegmenter,
-            rendered_image_cache::cache::RenderedImageCache,
-        },
-        types::{
-            editing_session::session::{CommonEditingSession, EditingSession},
-            inference_context_history::InferenceContextHistory,
-            point_history::PointHistory,
-            rendered_image::RenderedImage,
-        },
-        usecase::{
-            config::MAX_HISTORY,
-            upload_usecase::{
+            image_loader::loader::ImageLoader, preview_image_generator::PreviewImageGenerator, preview_storage::storage::PreviewStorage,
+        }, usecase::upload_usecase::{
                 error::UploadUseCaseError, upload_input::UploadInput, upload_output::UploadOutput,
             },
-        },
     }, domain::{
         entity::{original_image::OriginalImage, session::Session}, repository::{
             original_image_repository::repository::OriginalImageRepository,
             session_repository::repository::SessionRepository,
-        }, value_object::{image_id::image_id::ImageId, session_id::session_id::SessionId},
+        }, value_object::session_id::session_id::SessionId,
     },
 };
 
-pub struct UploadUseCase<SR, IR, LD, IC>
+pub struct UploadUseCase<SR, IR, LD, PS, PG>
 where
     SR: SessionRepository,
     IR: OriginalImageRepository,
     LD: ImageLoader,
-    IC: RenderedImageCache,
+    PS: PreviewStorage,
+    PG: PreviewImageGenerator,
 {
     session_repo: SR,
     image_repo: IR,
     loader: LD,
-    image_cache: IC,
+    preview_storage: PS,
+    preview_generator: PG,
 }
 
-impl<SR, IR, LD, IC> UploadUseCase<SR, IR, LD, IC>
+impl<SR, IR, LD, PS, PG> UploadUseCase<SR, IR, LD, PS, PG>
 where
     SR: SessionRepository,
     IR: OriginalImageRepository,
     LD: ImageLoader,
-    IC: RenderedImageCache,
+    PS: PreviewStorage,
+    PG: PreviewImageGenerator,
 {
     pub fn new(
         session_repository: SR,
         image_repository: IR,
         image_loader: LD,
-        rendered_image_cache: IC,
+        preview_storage: PS,
+        preview_generator: PG
     ) -> Self {
         Self {
             session_repo: session_repository,
             image_repo: image_repository,
             loader: image_loader,
-            image_cache: rendered_image_cache,
+            preview_storage: preview_storage,
+            preview_generator: preview_generator,
         }
     }
 
@@ -73,10 +66,8 @@ where
         let original_image = OriginalImage::new(image.clone());
         self.image_repo.save(original_image);
 
-        let (image_data, _, size) = image.into_data();
-
-        self.image_cache
-            .save(RenderedImage::new(image_data, image_id, size));
+        let preview = self.preview_generator.generate(image);
+        self.preview_storage.save(preview);
 
         let output = UploadOutput::new(session_id, image_id);
 
