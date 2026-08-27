ca3219d2a548c4c798b71ca850c7fb48dea8170e
ChickRen <chiaking207@icloud.com>
2026-08-27 14:04:41 +0900

    feat: refactor RedoUseCase to add Preview for improved performance

--- src/application/usecase/redo_usecase/usecase.rs
+++ src/application/usecase/redo_usecase/usecase.rs
@@ -1,25 +1,19 @@
 use crate::{
     application::{
         interface::{
-            editing_session_repository::repository::EditingSessionRepository,
-            image_segmenter::segmenter::ImageSegmenter,
-            rendered_image_cache::cache::RenderedImageCache,
-        },
-        types::{editing_session::session::EditingSession, rendered_image::RenderedImage},
-        usecase::redo_usecase::{
+            editing_session_repository::repository::EditingSessionRepository, image_segmenter::segmenter::ImageSegmenter, preview_image_generator::PreviewImageGenerator, preview_storage::storage::PreviewStorage,
+        }, types::editing_session::session::EditingSession, usecase::redo_usecase::{
             error::RedoUseCaseError, redo_input::RedoInput, redo_output::RedoOutput,
         },
-    },
-    domain::{
-        repository::{
+    }, domain::{
+        entity::image::Image, repository::{
             original_image_repository::repository::OriginalImageRepository,
             session_repository::repository::SessionRepository,
-        },
-        value_object::image_id::image_id::ImageId,
+        }, value_object::image_id::image_id::ImageId,
     },
 };
 
-pub struct RedoUseCase<SR, IR, IS, ESR, IC>
+pub struct RedoUseCase<SR, IR, IS, ESR, PS, PG>
 where
     SR: SessionRepository,
     IS: ImageSegmenter,
@@ -28,16 +22,18 @@ where
             StaticContext = <IS as ImageSegmenter>::StaticContext,
             InferenceContext = <IS as ImageSegmenter>::InferenceContext,
         >,
-    IC: RenderedImageCache,
+    PS: PreviewStorage,
+    PG: PreviewImageGenerator,
 {
     session_repo: SR,
     image_repo: IR,
     segmenter: IS,
     editing_session_repo: ESR,
-    image_cache: IC,
+    preview_storage: PS,
+    preview_generator: PG,
 }
 
-impl<SR, IR, IS, ESR, IC> RedoUseCase<SR, IR, IS, ESR, IC>
+impl<SR, IR, IS, ESR, PS, PG> RedoUseCase<SR, IR, IS, ESR, PS, PG>
 where
     SR: SessionRepository,
     IR: OriginalImageRepository,
@@ -46,21 +42,24 @@ where
             StaticContext = <IS as ImageSegmenter>::StaticContext,
             InferenceContext = <IS as ImageSegmenter>::InferenceContext,
         >,
-    IC: RenderedImageCache,
+    PS: PreviewStorage,
+    PG: PreviewImageGenerator,
 {
     pub fn new(
         session_repository: SR,
         image_repository: IR,
         image_segmenter: IS,
         editing_session_repository: ESR,
-        rendered_image_cache: IC,
+        preview_storage: PS,
+        preview_generator: PG
     ) -> Self {
         Self {
             session_repo: session_repository,
             image_repo: image_repository,
             segmenter: image_segmenter,
             editing_session_repo: editing_session_repository,
-            image_cache: rendered_image_cache,
+            preview_storage: preview_storage,
+            preview_generator: preview_generator,
         }
     }
 
@@ -78,20 +77,18 @@ where
         let static_context = editing_session.static_context();
         let inference_context = editing_session.inference_context();
 
-        let (_new_context, segmented_image) =
+        let (_, segmented_image) =
             self.segmenter
                 .segment(&original_image, static_context, inference_context, points)?;
 
-        let (segmented_image_data, size) = segmented_image.into_image_and_size();
-        let segmented_image_id = ImageId::new();
-
         self.editing_session_repo.save(&session_id, editing_session);
 
-        self.image_cache.save(RenderedImage::new(
-            segmented_image_data,
-            segmented_image_id,
-            size,
-        ));
+        let (segmented_image_data, size) = segmented_image.into_image_and_size();
+        let segmented_image_id = ImageId::new();
+        let segmenter_image = Image::new(segmented_image_data, segmented_image_id, size);
+
+        let preview_image = self.preview_generator.generate(&segmenter_image);
+        self.preview_storage.save(preview_image);
 
         let output = RedoOutput::new(segmented_image_id);
         Ok(output)
