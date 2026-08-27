044409a8430029e600e77adc891d457ecd6e7bc3
ChickRen <chiaking207@icloud.com>
2026-08-27 15:35:50 +0900

    refactor: remove rendered_image_cache

--- src/application/interface.rs
+++ src/application/interface.rs
@@ -1,7 +1,6 @@
 pub mod editing_session_repository;
 pub mod image_loader;
 pub mod image_segmenter;
-pub mod rendered_image_cache;
 pub mod preview_storage;
 pub mod segmenter_input_image_storage;
 pub mod preview_image_generator;
--- src/application/interface/rendered_image_cache/error.rs
+++
@@ -1,7 +0,0 @@
-use crate::{common::traits::ErrorType::NotFound, leaf_error};
-
-leaf_error!(
-    pub enum RenderedCacheError {
-        ImageNotFound => ("IMAGE_NOT_FOUND", NotFound),
-    }
-);
