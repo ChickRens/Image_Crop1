e80f32c45f53ea7433560aa896ce53a03f49c02f
ChickRen <chiaking207@icloud.com>
2026-08-29 12:24:29 +0900

    feat: refactor SegmentUseCase to integrate SegmenterInputImageStorage for improved image processing

--- src/application/usecase/segment_usecase/usecase.rs
+++ src/application/usecase/segment_usecase/usecase.rs
@@ -1,22 +1,20 @@
 use crate::{
     application::{
         interface::{
-            editing_session_repository::repository::EditingSessionRepository, image_segmenter::segmenter::ImageSegmenter, preview_image_generator::PreviewImageGenerator, preview_storage::storage::PreviewStorage,
+            editing_session_repository::repository::EditingSessionRepository, image_segmenter::segmenter::ImageSegmenter, preview_image_generator::PreviewImageGenerator, preview_storage::storage::PreviewStorage, segmenter_input_image_storage::storage::SegmenterInputImageStorage,
         }, types::editing_session::session::EditingSession, usecase::segment_usecase::{
             error::SegmentUseCaseError, segment_input::SegmentInput, segment_output::SegmentOutput,
         },
     }, domain::{
         entity::image::Image, repository::{
-            original_image_repository::repository::OriginalImageRepository,
             session_repository::repository::SessionRepository,
         }, value_object::image_id::image_id::ImageId,
     },
 };
 
-pub struct SegmentUseCase<SR, IR, IS, ESR, PS, PG>
+pub struct SegmentUseCase<SR, IS, ESR, PS, PG, SS>
 where
     SR: SessionRepository,
-    IR: OriginalImageRepository,
     IS: ImageSegmenter,
     ESR: EditingSessionRepository<
             StaticContext = IS::StaticContext,
@@ -24,19 +22,19 @@ where
         >,
     PS: PreviewStorage,
     PG: PreviewImageGenerator,
+    SS: SegmenterInputImageStorage,
 {
     session_repo: SR,
-    image_repo: IR,
     segmenter: IS,
     editing_session_repo: ESR,
     preview_storage: PS,
     preview_generator: PG,
+    input_storage: SS,
 }
 
-impl<SR, IR, IS, ESR, PS, PG> SegmentUseCase<SR, IR, IS, ESR, PS, PG>
+impl<SR, IS, ESR, PS, PG, SS> SegmentUseCase<SR, IS, ESR, PS, PG, SS>
 where
     SR: SessionRepository,
-    IR: OriginalImageRepository,
     IS: ImageSegmenter,
     ESR: EditingSessionRepository<
             StaticContext = IS::StaticContext,
@@ -44,22 +42,23 @@ where
         >,
     PS: PreviewStorage,
     PG: PreviewImageGenerator,
+    SS: SegmenterInputImageStorage,
 {
     pub fn new(
         session_repository: SR,
-        image_repository: IR,
         image_segmenter: IS,
         editing_session_repository: ESR,
         preview_storage: PS,
-        preview_generator: PG
+        preview_generator: PG,
+        segmenter_input_storage: SS,
     ) -> Self {
         Self {
             session_repo: session_repository,
-            image_repo: image_repository,
             segmenter: image_segmenter,
             editing_session_repo: editing_session_repository,
             preview_storage: preview_storage,
             preview_generator: preview_generator,
+            input_storage: segmenter_input_storage,
         }
     }
 
@@ -68,7 +67,7 @@ where
         let session = self.session_repo.get(&session_id)?;
         let image_id = session.image_id();
 
-        let original_image = self.image_repo.get(image_id)?;
+        let original_input_image = self.input_storage.get(*image_id)?;
 
         let mut editing_session = self.editing_session_repo.get(&session_id)?;
 
@@ -78,7 +77,7 @@ where
 
         let (new_inference_context, segmented_image) =
             self.segmenter
-                .segment(&original_image, static_context, inference_context, points)?;
+                .segment(&original_input_image, static_context, inference_context, points)?;
 
         let (segmented_image_data, size) = segmented_image.into_image_and_size();
         let segmented_image_id = ImageId::new();
