c21b79d46ebe105f3baf1c8accf2f94295861116
ChickRen <chiaking207@icloud.com>
2026-08-29 11:57:04 +0900

    feat: add SharedPreviewGenerator for managing shared preview images

--- src/infrastructure/generator.rs
+++ src/infrastructure/generator.rs
@@ -1,2 +1,3 @@
 pub mod preview;
 pub mod segmenter_input;
+pub mod shared_preview;
---
+++ src/infrastructure/generator/shared_preview.rs
@@ -0,0 +1,19 @@
+use std::sync::Arc;
+
+use crate::{application::{interface::preview_image_generator::PreviewImageGenerator, types::preview_image::PreviewImage}, domain::entity::image::Image, infrastructure::generator::preview::WebPPreviewImageGenerator};
+
+pub struct SharedPreviewGenerator {
+    generator: Arc<WebPPreviewImageGenerator>
+}
+
+impl PreviewImageGenerator for SharedPreviewGenerator {
+    fn generate(&self, image: &Image) -> PreviewImage {
+        self.generator.generate(image)
+    }
+}
+
+impl SharedPreviewGenerator {
+    pub fn new(preview_image_long_side: u16) -> Self {
+        Self { generator: Arc::new(WebPPreviewImageGenerator::new(preview_image_long_side)) }
+    }
+}
