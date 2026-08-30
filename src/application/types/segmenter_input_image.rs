6fd2e9d8849b012c8425a5f27e3b7ccd7ab33205
ChickRen <chiaking207@icloud.com>
2026-08-30 17:09:55 +0900

    feat: add getter method for image in SegmenterInputImage

--- src/application/types/segmenter_input_image.rs
+++ src/application/types/segmenter_input_image.rs
@@ -10,6 +10,10 @@ impl SegmenterInputImage {
         Self { image }
     }
 
+    pub fn image(&self) -> &Image {
+        &self.image
+    }
+
     pub fn image_id(&self) -> &ImageId {
         self.image.image_id()
     }
