2d69638234d4ba6b1ec24fbec464eab0ee5cca22
ChickRen <chiaking207@icloud.com>
2026-09-01 15:46:09 +0900

    feat: refactor GetImageUseCase to integrate PreviewService and improve structure

--- src/application/usecase/get_image_usecase/usecase.rs
+++ src/application/usecase/get_image_usecase/usecase.rs
@@ -1,29 +1,31 @@
 use crate::application::{
-    interface::preview_storage::storage::PreviewStorage, usecase::get_image_usecase::{
+    interface::{preview_image_generator::PreviewImageGenerator, preview_storage::storage::PreviewStorage}, service::preview_service::PreviewService, usecase::get_image_usecase::{
         error::GetImageUseCaseError, get_image_input::GetImageInput,
         get_image_output::GetImageOutput,
     },
 };
 
-pub struct GetImageUseCase<PS>
+pub struct GetImageUseCase<PG, PS>
 where
-    PS: PreviewStorage
-{
-    storage: PS,
-}
-
-impl<PS> GetImageUseCase<PS>
-where
+    PG: PreviewImageGenerator,
     PS: PreviewStorage,
 {
-    pub fn new(preview_storage: PS) -> Self {
-        Self { storage: preview_storage }
+    preview_service: PreviewService<PG, PS>
+}
+
+impl<PG, PS> GetImageUseCase<PG, PS>
+where
+    PG: PreviewImageGenerator,
+    PS: PreviewStorage,
+{
+    pub fn new(preview_service: PreviewService<PG, PS>) -> Self {
+        Self { preview_service }
     }
 
     pub fn execute(&self, input: GetImageInput) -> Result<GetImageOutput, GetImageUseCaseError> {
         let image_id = input.image_id();
 
-        let preview = self.storage.get(image_id)?;
+        let preview = self.preview_service.get(image_id)?;
 
         let (data, _, size) = preview.into_data();
 
