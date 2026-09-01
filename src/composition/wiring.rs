25881a322aa80f032d3a5a37ff1a439fdd9557a6
ChickRen <chiaking207@icloud.com>
2026-09-01 22:51:54 +0900

    feat: refactor wiring to improve service integration and processing speed

--- src/composition/wiring.rs
+++ src/composition/wiring.rs
@@ -1,30 +1,21 @@
 use crate::{
     application::{
-        error::ApplicationError,
-        usecase::{
+        error::ApplicationError, service::{prepare_service::SharedPrepareSegmentService, preview_service::SharedPreviewService, segment_service::SharedSegmentService}, usecase::{
             get_image_usecase::{
                 get_image_input::GetImageInput, get_image_output::GetImageOutput,
                 usecase::GetImageUseCase,
-            },
-            redo_usecase::{redo_input::RedoInput, redo_output::RedoOutput, usecase::RedoUseCase},
-            segment_usecase::{
+            }, prepare_segment_usecase::usecase::PrepareSegmentUseCase, redo_usecase::{redo_input::RedoInput, redo_output::RedoOutput, usecase::RedoUseCase}, segment_usecase::{
                 segment_input::SegmentInput, segment_output::SegmentOutput, usecase::SegmentUseCase,
-            },
-            undo_usecase::{undo_input::UndoInput, undo_output::UndoOutput, usecase::UndoUseCase},
-            upload_usecase::{
+            }, undo_usecase::{undo_input::UndoInput, undo_output::UndoOutput, usecase::UndoUseCase}, upload_usecase::{
                 upload_input::UploadInput, upload_output::UploadOutput, usecase::UploadUseCase,
             },
         },
-    },
-    infrastructure::{
-        cache::shared_rendered_image_cache::SharedRenderedImageCacheInMemory,
-        image_loader::FileImageLoader,
-        repository::{
+    }, infrastructure::{
+        generator::{shared_preview::SharedPreviewGenerator, shared_segmenter_input::SharedSAM2InputGenerator}, image_loader::FileImageLoader, repository::{
             shared_editing_session_repository::SharedEditingSessionRepository,
             shared_image_repository::SharedOriginalImageRepository,
             shared_session_repository::SharedSessionRepository,
-        },
-        segmenter::shared_sam2::SharedSAM2Segmenter,
+        }, segmenter::shared_sam2::SharedSAM2Segmenter, storage::{shared_preview_storage::SharedPreviewStorage, shared_segmenter_input_storage::SharedSegmenterInputStorage},
     },
 };
 
@@ -33,32 +24,65 @@ pub struct App {
         SharedSessionRepository,
         SharedOriginalImageRepository,
         FileImageLoader,
-        SharedSAM2Segmenter,
-        SharedEditingSessionRepository,
-        SharedRenderedImageCacheInMemory,
+        SharedPreviewService<
+            SharedPreviewGenerator,
+            SharedPreviewStorage,
+        >
+    >,
+    prepare_segment_usecase: PrepareSegmentUseCase<
+        SharedPrepareSegmentService<
+            SharedSAM2Segmenter,
+            SharedOriginalImageRepository,
+            SharedEditingSessionRepository,
+            SharedSAM2InputGenerator,
+            SharedSegmenterInputStorage,
+        >
     >,
     segment_usecase: SegmentUseCase<
-        SharedSessionRepository,
-        SharedOriginalImageRepository,
-        SharedSAM2Segmenter,
-        SharedEditingSessionRepository,
-        SharedRenderedImageCacheInMemory,
+        SharedSegmentService<
+            SharedSessionRepository,
+            SharedSAM2Segmenter,
+            SharedOriginalImageRepository,
+            SharedEditingSessionRepository,
+            SharedSegmenterInputStorage,
+        >,
+        SharedPreviewService<
+            SharedPreviewGenerator,
+            SharedPreviewStorage,
+        >
     >,
     undo_usecase: UndoUseCase<
-        SharedSessionRepository,
-        SharedOriginalImageRepository,
-        SharedSAM2Segmenter,
-        SharedEditingSessionRepository,
-        SharedRenderedImageCacheInMemory,
+        SharedSegmentService<
+            SharedSessionRepository,
+            SharedSAM2Segmenter,
+            SharedOriginalImageRepository,
+            SharedEditingSessionRepository,
+            SharedSegmenterInputStorage,
+        >,
+        SharedPreviewService<
+            SharedPreviewGenerator,
+            SharedPreviewStorage,
+        >
     >,
     redo_usecase: RedoUseCase<
-        SharedSessionRepository,
-        SharedOriginalImageRepository,
-        SharedSAM2Segmenter,
-        SharedEditingSessionRepository,
-        SharedRenderedImageCacheInMemory,
+        SharedPreviewService<
+            SharedPreviewGenerator,
+            SharedPreviewStorage,
+        >,
+        SharedSegmentService<
+            SharedSessionRepository,
+            SharedSAM2Segmenter,
+            SharedOriginalImageRepository,
+            SharedEditingSessionRepository,
+            SharedSegmenterInputStorage,
+        >,
     >,
-    get_image_usecase: GetImageUseCase<SharedRenderedImageCacheInMemory>,
+    get_image_usecase: GetImageUseCase<
+        SharedPreviewService<
+            SharedPreviewGenerator,
+            SharedPreviewStorage,
+        >,
+    >
 }
 
 impl App {
@@ -69,41 +93,25 @@ impl App {
         let segmenter = SharedSAM2Segmenter::new("models")
             .expect("Failed to load SAM2 model during app initialize");
         let editing_session_repo = SharedEditingSessionRepository::new();
-        let image_cache = SharedRenderedImageCacheInMemory::new();
+        let preview_storage = SharedPreviewStorage::new();
+        let preview_generator = SharedPreviewGenerator::new(600);
+        let input_storage = SharedSegmenterInputStorage::new();
+        let input_generator = SharedSAM2InputGenerator::new(1024, 1024);
 
-        let upload_uc = UploadUseCase::new(
-            session_repo.clone(),
-            image_repo.clone(),
-            loader,
-            segmenter.clone(),
-            editing_session_repo.clone(),
-            image_cache.clone(),
-        );
-        let segment_uc = SegmentUseCase::new(
-            session_repo.clone(),
-            image_repo.clone(),
-            segmenter.clone(),
-            editing_session_repo.clone(),
-            image_cache.clone(),
-        );
-        let undo_uc = UndoUseCase::new(
-            session_repo.clone(),
-            image_repo.clone(),
-            segmenter.clone(),
-            editing_session_repo.clone(),
-            image_cache.clone(),
-        );
-        let redo_uc = RedoUseCase::new(
-            session_repo.clone(),
-            image_repo.clone(),
-            segmenter.clone(),
-            editing_session_repo.clone(),
-            image_cache.clone(),
-        );
-        let get_image_uc = GetImageUseCase::new(image_cache.clone());
+        let preview_service = SharedPreviewService::new(preview_generator, preview_storage);
+        let prepare_service = SharedPrepareSegmentService::new(segmenter.clone(), image_repo.clone(), editing_session_repo.clone(), input_generator, input_storage.clone(), 50);
+        let segment_service = SharedSegmentService::new(session_repo.clone(), segmenter.clone(), image_repo.clone(), editing_session_repo.clone(), input_storage.clone());
+
+        let upload_uc = UploadUseCase::new(session_repo.clone(), image_repo.clone(), loader, preview_service.clone());
+        let prepare_segment_uc = PrepareSegmentUseCase::new(prepare_service);
+        let segment_uc = SegmentUseCase::new(segment_service.clone(), preview_service.clone());
+        let undo_uc = UndoUseCase::new(segment_service.clone(), preview_service.clone());
+        let redo_uc = RedoUseCase::new(segment_service.clone(), preview_service.clone());
+        let get_image_uc = GetImageUseCase::new(preview_service.clone());
 
         Ok(App {
             upload_usecase: upload_uc,
+            prepare_segment_usecase: prepare_segment_uc,
             segment_usecase: segment_uc,
             undo_usecase: undo_uc,
             redo_usecase: redo_uc,
