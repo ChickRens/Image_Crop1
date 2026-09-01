af543f62a74979f7ab1673bd4006a3944bd153c7
ChickRen <chiaking207@icloud.com>
2026-09-01 19:05:08 +0900

    feat: refactor PrepareSegmentUseCase to integrate PrepareSegmentService

--- src/application/usecase/prepare_segment_usecase/error.rs
+++ src/application/usecase/prepare_segment_usecase/error.rs
@@ -1,8 +1,7 @@
-use crate::{application::interface::image_segmenter::error::SegmenterLoadingError, domain::repository::original_image_repository::error::OriginalImageRepositoryError, parent_error};
+use crate::{application::service::error::PrepareServiceError, parent_error};
 
 parent_error!(
     pub enum PrepareSegmentUseCaseError {
-        ImageRepository(OriginalImageRepositoryError),
-        Segmenter(SegmenterLoadingError),
+        PrepareSegmentService(PrepareServiceError),
     }
 );
--- src/application/usecase/prepare_segment_usecase/usecase.rs
+++ src/application/usecase/prepare_segment_usecase/usecase.rs
@@ -1,4 +1,4 @@
-use crate::{application::{interface::{editing_session_repository::repository::EditingSessionRepository, image_segmenter::segmenter::ImageSegmenter, segmenter_input_image_generator::SegmenterInputImageGenerator, segmenter_input_image_storage::storage::SegmenterInputImageStorage}, types::{editing_session::session::CommonEditingSession, inference_context_history::InferenceContextHistory, point_history::PointHistory}, usecase::{config::MAX_HISTORY, prepare_segment_usecase::{error::PrepareSegmentUseCaseError, prepare_segment_input::PrepareSegmentInput}}}, domain::repository::original_image_repository::repository::OriginalImageRepository};
+use crate::{application::{interface::{editing_session_repository::repository::EditingSessionRepository, image_segmenter::segmenter::ImageSegmenter, segmenter_input_image_generator::SegmenterInputImageGenerator, segmenter_input_image_storage::storage::SegmenterInputImageStorage}, service::prepare_service::PrepareSegmentService, usecase::{prepare_segment_usecase::{error::PrepareSegmentUseCaseError, prepare_segment_input::PrepareSegmentInput}}}, domain::repository::original_image_repository::repository::OriginalImageRepository};
 
 pub struct PrepareSegmentUseCase<IR, IS, ESR, SS, SG>
 where
@@ -11,11 +11,7 @@ where
     SS: SegmenterInputImageStorage,
     SG: SegmenterInputImageGenerator,
 {
-    image_repo: IR,
-    segmenter: IS,
-    editing_session_repo: ESR,
-    input_storage: SS,
-    input_generator: SG,
+    prepare_service: PrepareSegmentService<IS, IR, ESR, SG, SS>
 }
 
 impl<IR, IS, ESR, SS, SG> PrepareSegmentUseCase<IR, IS, ESR, SS, SG>
@@ -30,18 +26,10 @@ where
     SG: SegmenterInputImageGenerator,
 {
     pub fn new(
-        image_repository: IR,
-        image_segmenter: IS,
-        editing_session_repository: ESR,
-        segmenter_input_image_storage: SS,
-        segmenter_input_image_generator: SG,
+        prepare_service: PrepareSegmentService<IS, IR, ESR, SG, SS>
     ) -> Self {
         Self {
-            image_repo: image_repository,
-            segmenter: image_segmenter,
-            editing_session_repo: editing_session_repository,
-            input_storage: segmenter_input_image_storage,
-            input_generator: segmenter_input_image_generator,
+            prepare_service
         }
     }
 
@@ -50,20 +38,7 @@ where
         let session_id = input.session_id();
         let preview_to_original_scale = input.point_scale();
 
-        let original_image = self.image_repo.get(&image_id)?;
-        let segmenter_input = self.input_generator.generate(&original_image);
-
-        let static_context = self.segmenter.prepare_static_context(&segmenter_input)?;
-        let inference_context = self.segmenter.prepare_inference_context(&segmenter_input)?;
-
-        let point_history = PointHistory::new(MAX_HISTORY);
-        let inference_context_history = InferenceContextHistory::new(MAX_HISTORY);
-
-        let editing_session = CommonEditingSession::new(point_history, static_context, inference_context_history, inference_context, preview_to_original_scale);
-        self.editing_session_repo.save(&session_id, editing_session);
-
-        self.input_storage.save(segmenter_input);
-
+        self.prepare_service.prepare(session_id, image_id, preview_to_original_scale)?;
         Ok(())
     }
 }
