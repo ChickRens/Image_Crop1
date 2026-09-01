ea737a895f85a0694d5b3e6f230ad4e26dbc3823
ChickRen <chiaking207@icloud.com>
2026-09-01 15:03:23 +0900

    feat: refactor RedoUseCase to improve error handling and integrate SegmentService

--- src/application/usecase/redo_usecase/error.rs
+++ src/application/usecase/redo_usecase/error.rs
@@ -1,20 +1,14 @@
 use crate::{
     application::{
         interface::{
-            editing_session_repository::error::EditingSessionRepositoryError, image_segmenter::error::SegmenterRuntimeError,
-        }, types::editing_session::error::EditingSessionError,
-    }, domain::repository::{
-        original_image_repository::error::OriginalImageRepositoryError,
-        session_repository::error::SessionRepositoryError,
+            segmenter_input_image_storage::error::SegmenterInputImageStorageError,
+        }, service::error::SegmentServiceError,
     }, parent_error,
 };
 
 parent_error!(
     pub enum RedoUseCaseError {
-        ImageRepository(OriginalImageRepositoryError),
-        SessionRepository(SessionRepositoryError),
-        EditingSessionRepository(EditingSessionRepositoryError),
-        Segmenter(SegmenterRuntimeError),
-        EditingSession(EditingSessionError),
+        SegmentService(SegmentServiceError),
+        SegmenterInputStorage(SegmenterInputImageStorageError),
     }
 );
--- src/application/usecase/redo_usecase/usecase.rs
+++ src/application/usecase/redo_usecase/usecase.rs
@@ -1,19 +1,27 @@
 use crate::{
     application::{
         interface::{
-            editing_session_repository::repository::EditingSessionRepository, image_segmenter::segmenter::ImageSegmenter, preview_image_generator::PreviewImageGenerator, preview_storage::storage::PreviewStorage,
-        }, types::editing_session::session::EditingSession, usecase::redo_usecase::{
-            error::RedoUseCaseError, redo_input::RedoInput, redo_output::RedoOutput,
+            editing_session_repository::repository::EditingSessionRepository,
+            image_segmenter::segmenter::ImageSegmenter,
+            preview_image_generator::PreviewImageGenerator,
+            preview_storage::storage::PreviewStorage,
+            segmenter_input_image_storage::storage::SegmenterInputImageStorage,
+        }, service::{preview_service::PreviewService, segment_service::SegmentService}, types::editing_session::session::EditingSession, usecase::redo_usecase::{
+            error::RedoUseCaseError,
+            redo_input::RedoInput,
+            redo_output::RedoOutput,
         },
     }, domain::{
-        entity::image::Image, repository::{
+        entity::image::Image,
+        repository::{
             original_image_repository::repository::OriginalImageRepository,
             session_repository::repository::SessionRepository,
-        }, value_object::image_id::image_id::ImageId,
+        },
+        value_object::image_id::image_id::ImageId,
     },
 };
 
-pub struct RedoUseCase<SR, IR, IS, ESR, PS, PG>
+pub struct RedoUseCase<SR, IR, IS, ESR, PS, PG, SS>
 where
     SR: SessionRepository,
     IS: ImageSegmenter,
@@ -24,16 +32,13 @@ where
         >,
     PS: PreviewStorage,
     PG: PreviewImageGenerator,
+    SS: SegmenterInputImageStorage,
 {
-    session_repo: SR,
-    image_repo: IR,
-    segmenter: IS,
-    editing_session_repo: ESR,
-    preview_storage: PS,
-    preview_generator: PG,
+    segment_service: SegmentService<SR, IS, IR, ESR, SS>,
+    preview_service: PreviewService<PG, PS>,
 }
 
-impl<SR, IR, IS, ESR, PS, PG> RedoUseCase<SR, IR, IS, ESR, PS, PG>
+impl<SR, IR, IS, ESR, PS, PG, SS> RedoUseCase<SR, IR, IS, ESR, PS, PG, SS>
 where
     SR: SessionRepository,
     IR: OriginalImageRepository,
@@ -44,51 +49,24 @@ where
         >,
     PS: PreviewStorage,
     PG: PreviewImageGenerator,
+    SS: SegmenterInputImageStorage,
 {
     pub fn new(
-        session_repository: SR,
-        image_repository: IR,
-        image_segmenter: IS,
-        editing_session_repository: ESR,
-        preview_storage: PS,
-        preview_generator: PG
+        preview_service: PreviewService<PG, PS>,
+        segment_service: SegmentService<SR, IS, IR, ESR, SS>
     ) -> Self {
         Self {
-            session_repo: session_repository,
-            image_repo: image_repository,
-            segmenter: image_segmenter,
-            editing_session_repo: editing_session_repository,
-            preview_storage: preview_storage,
-            preview_generator: preview_generator,
+            preview_service,
+            segment_service
         }
     }
 
     pub fn execute(&self, redo_input: RedoInput) -> Result<RedoOutput, RedoUseCaseError> {
         let session_id = redo_input.session_id();
-        let session = self.session_repo.get(&session_id)?;
+        let segmented_image = self.segment_service.redo(session_id)?;
 
-        let original_image_id = session.image_id();
-        let original_image = self.image_repo.get(original_image_id)?;
-
-        let mut editing_session = self.editing_session_repo.get(&session_id)?;
-
-        editing_session.redo()?;
-        let points = editing_session.points();
-        let static_context = editing_session.static_context();
-        let inference_context = editing_session.inference_context();
-
-        let (_, segmented_image) =
-            self.segmenter
-                .segment(&original_image, static_context, inference_context, points)?;
-
-        self.editing_session_repo.save(&session_id, editing_session);
-
-        let (segmented_image_data, size) = segmented_image.into_image_and_size();
-        let segmented_image_id = ImageId::new();
-        let segmenter_image = Image::new(segmented_image_data, segmented_image_id, size);
-
-        let preview_image = self.preview_generator.generate(&segmenter_image);
-        self.preview_storage.save(preview_image);
+        self.preview_service.generate_and_save(&segmented_image);
+        let segmented_image_id = *segmented_image.image_id();
 
         let output = RedoOutput::new(segmented_image_id);
         Ok(output)
