5c0e96b302700abce9bbcdc15a2c26871716f988
ChickRen <chiaking207@icloud.com>
2026-09-02 12:42:09 +0900

    feat: optimize image retrieval by removing unnecessary conversions and changing response format to webp

--- src/presentation/handler/get_image/handler.rs
+++ src/presentation/handler/get_image/handler.rs
@@ -1,7 +1,6 @@
-use std::{io::Cursor, sync::Arc};
+use std::{sync::Arc};
 
 use axum::{Json, body::Body, extract::State, response::Response};
-use image::{RgbImage, RgbaImage};
 
 use crate::{application::usecase::get_image_usecase::get_image_input::GetImageInput, composition::wiring::App, domain::value_object::image_id::image_id::ImageId, presentation::{errors::{app_error::AppError, presentation_error::PresentationError}, handler::get_image::request::GetImageRequest}};
 
@@ -10,7 +9,7 @@ pub async fn get_image(
     State(app): State<Arc<App>>,
     Json(request): Json<GetImageRequest>
 ) -> Result<Response, AppError>{
-    println!("Getting image");
+    println!("Getting image start");
     let image_id = request.image_id;
     let image_id = ImageId::from_str(&image_id)
         .map_err(|err| PresentationError::from(err))?;
@@ -19,17 +18,10 @@ pub async fn get_image(
     
     let output = app.get_image(input)?;
     
-    let (image, size) = output.into_image_data();
-    let rgba_image = RgbaImage::from_raw(size.width() as u32, size.height() as u32, image)
-    .ok_or(PresentationError::Loading)?;
-
-    let mut bytes = Vec::new();
-    let mut cursor = Cursor::new(&mut bytes);
-    
-    rgba_image.write_to(&mut cursor, image::ImageFormat::Png).map_err(|err| PresentationError::ConvertToPng(err.to_string()))?;
+    let (image, _) = output.image_data();
 
     Ok(Response::builder()
-        .header("Content-Type", "image/png")
-        .body(Body::from(bytes))
+        .header("Content-Type", "image/webp")
+        .body(Body::from(image))
         .unwrap())
 }
