92f770df1039a9635f94e2fa1605143898a6811c
ChickRen <chiaking207@icloud.com>
2026-09-05 14:49:23 +0900

    feat: enhance mask resizing performance with parallel processing

--- src/infrastructure/segmenter/mask_resizer.rs
+++ src/infrastructure/segmenter/mask_resizer.rs
@@ -1,21 +1,21 @@
 use ndarray::{Array4, ArrayView4, Axis};
+use rayon::{iter::{IndexedParallelIterator, ParallelIterator}, slice::ParallelSliceMut};
 
 pub struct SAM2MaskResizer;
 
 impl SAM2MaskResizer {
     pub fn resize_mask(mask: &ArrayView4<f32>, target_h: usize, target_w: usize) -> Array4<f32> {
         let (batch, channel, src_h, src_w) = mask.dim();
+
+        debug_assert_eq!(batch, 1);
+        debug_assert_eq!(channel, 1);
+
         let mask = mask.index_axis(Axis(0), 0);
         let mask = mask.index_axis(Axis(0), 0);
 
         let input = mask
             .as_slice()
             .expect("Resizer requires contiguous ArrayView4");
-
-        println!("batch       : {:?}", batch);
-        println!("channel     : {:?}", channel);
-        println!("mask_height : {:?}", src_h);
-        println!("mask_width  : {:?}", src_w);
 
         let mut output = Array4::<f32>::zeros((1, 1, target_h, target_w));
         let mut output_view = output.index_axis_mut(Axis(0), 0);
@@ -25,41 +25,42 @@ impl SAM2MaskResizer {
             .as_slice_mut()
             .expect("Resizer requires contiguous ArrayView4");
 
-        for y in 0..target_h {
-            let src_y = (y as f32 + 0.5) * src_h as f32 / target_h as f32 - 0.5;
-            let src_y = src_y.clamp(0.0, (src_h - 1) as f32);
+        output_slice
+            .par_chunks_exact_mut(target_w)
+            .enumerate()
+            .for_each(|(y, row)|
+            {
+                let src_y = (y as f32 + 0.5) * src_h as f32 / target_h as f32 - 0.5;
+                let src_y = src_y.clamp(0.0, (src_h - 1) as f32);
 
-            let upper_left_y = src_y.floor() as usize;
-            let lower_right_y = (upper_left_y + 1).min(src_h - 1);
-            let dy = src_y - upper_left_y as f32;
+                let upper_y = src_y.floor() as usize;
+                let lower_y = (upper_y + 1).min(src_h - 1);
+                let dy = src_y - upper_y as f32;
 
-            for x in 0..target_w {
-                let src_x = (x as f32 + 0.5) * src_w as f32 / target_w as f32 - 0.5;
-                let src_x = src_x.clamp(0.0, (src_w - 1) as f32);
+                let upper_row = upper_y * src_w;
+                let lower_row = lower_y * src_w;
 
-                let upper_left_x = src_x.floor() as usize;
-                let lower_right_x = (upper_left_x + 1).min(src_w - 1);
+                for x in 0..target_w {
+                    let src_x = (x as f32 + 0.5) * src_w as f32 / target_w as f32 - 0.5;
+                    let src_x = src_x.clamp(0.0, (src_h - 1) as f32);
 
-                let dx = src_x - upper_left_x as f32;
+                    let left_x = src_x.floor() as usize;
+                    let right_x = (left_x + 1).min(src_w - 1);
 
-                let upper_row = upper_left_y * src_w;
-                let lower_row = lower_right_y * src_w;
+                    let dx = src_x - left_x as f32;
 
-                let upper_left = input[upper_row + upper_left_x];
-                let upper_right = input[upper_row + lower_right_x];
-                let lower_left = input[lower_row + upper_left_x];
-                let lower_right = input[lower_row + lower_right_x];
+                    let upper_left = input[upper_row + left_x];
+                    let upper_right = input[upper_row + right_x];
+                    let lower_left = input[lower_row + left_x];
+                    let lower_right = input[lower_row + right_x];
 
-                let value = (1.0 - dx) * (1.0 - dy) * upper_left
-                    + dx * (1.0 - dy) * upper_right
-                    + (1.0 - dx) * dy * lower_left
-                    + dx * dy * lower_right;
+                    row[x] = (1.0 - dx) * (1.0 - dy) * upper_left
+                        + dx * (1.0 - dy) * upper_right
+                        + (1.0 - dx) * dy * lower_left
+                        + dx * dy * lower_right;
+                }
+            });
 
-                output_slice[y * target_w + x] = value
-                // output[[0, 0, y, x]] = mask[[0, 0, src_y, src_x]];
-            }
-        }
-
-        output
+            output
     }
 }
