4442a944e818eb1aa773d9e61d4b4a6b17a598ab
ChickRen <chiaking207@icloud.com>
2026-09-06 18:21:48 +0900

    feat: add completed_image_repository with error handling and repository trait

--- src/domain/repository.rs
+++ src/domain/repository.rs
@@ -1,2 +1,3 @@
 pub mod original_image_repository;
 pub mod session_repository;
+pub mod completed_image_repository;
---
+++ src/domain/repository/completed_image_repository/repository.rs
@@ -0,0 +1,6 @@
+use crate::domain::{entity::completed_image::CompletedImage, repository::completed_image_repository::error::CompletedImageRepositoryError, value_object::image_id::image_id::ImageId};
+
+pub trait CompletedImageRepository {
+    fn save(&self, completed_image: CompletedImage);
+    fn get(&self, image_id: ImageId) -> Result<CompletedImage, CompletedImageRepositoryError>;
+}
