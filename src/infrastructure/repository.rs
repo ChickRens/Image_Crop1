98bcde4d8125fb1e37203a8a5d0ee5d577fcd706
ChickRen <chiaking207@icloud.com>
2026-09-07 10:10:29 +0900

    feat: add CompletedRepositoryInMemory

--- src/infrastructure/repository.rs
+++ src/infrastructure/repository.rs
@@ -4,3 +4,4 @@ pub mod session_repository;
 pub mod shared_editing_session_repository;
 pub mod shared_image_repository;
 pub mod shared_session_repository;
+pub mod completed_repository;
---
+++ src/infrastructure/repository/completed_repository.rs
@@ -0,0 +1,20 @@
+use std::{collections::HashMap, sync::RwLock};
+
+use crate::domain::{entity::completed_image::CompletedImage, repository::completed_image_repository::{error::CompletedImageRepositoryError, repository::CompletedImageRepository}, value_object::image_id::image_id::ImageId};
+
+pub struct CompletedRepositoryInMemory {
+    image: RwLock<HashMap<ImageId, CompletedImage>>
+}
+
+impl CompletedImageRepository for CompletedRepositoryInMemory {
+    fn save(&self, completed_image: CompletedImage) {
+        let mut image = self.image.write().expect("CompletedImageRepository is Poisoned");
+        let image_id = completed_image.image_id();
+        image.insert(image_id, completed_image);
+    }
+
+    fn get(&self, image_id: ImageId) -> Result<CompletedImage, CompletedImageRepositoryError> {
+        let image = self.image.read().expect("CompletedImageRepository is Poisoned");
+        image.get(&image_id).cloned().ok_or(CompletedImageRepositoryError::ImageNotFound)
+    }
+}
