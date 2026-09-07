c7e7c2adaec9b30ea7eaf2c6ebf4ef0e1bde5672
ChickRen <chiaking207@icloud.com>
2026-09-07 18:19:39 +0900

    feat: add get_completed handler and request structure

---
+++ src/presentation/handler/get_completed/handler.rs
@@ -0,0 +1,26 @@
+use std::sync::Arc;
+
+use axum::{Json, body::Body, extract::State, response::Response};
+
+use crate::{application::usecase::get_completed_usecase::input::GetCompletedInput, composition::wiring::App, domain::value_object::image_id::image_id::ImageId, presentation::{errors::{app_error::AppError, presentation_error::PresentationError}, handler::get_completed::request::GetCompletedRequest}};
+
+#[axum::debug_handler]
+pub async fn get_completed_image(
+    State(app): State<Arc<App>>,
+    Json(request): Json<GetCompletedRequest>,
+) -> Result<Response, AppError> {
+    println!("Getting image start");
+    let image_id = request.image_id;
+    let image_id = ImageId::from_str(&image_id).map_err(|err| PresentationError::from(err))?;
+
+    let input = GetCompletedInput::new(image_id);
+
+    let output = app.get_completed_image(input)?;
+
+    let (image, _) = output.image_data();
+
+    Ok(Response::builder()
+        .header("Content-Type", "image/webp")
+        .body(Body::from(image))
+        .unwrap())
+}
---
+++ src/presentation/handler/get_completed/request.rs
@@ -0,0 +1,4 @@
+#[derive(serde::Deserialize)]
+pub struct GetCompletedRequest {
+    pub image_id: String
+}
