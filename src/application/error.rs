35b90c7c2c0dadebc593720d3bc5f8468b8ff9ef
ChickRen <chiaking207@icloud.com>
2026-09-07 18:07:05 +0900

    feat: update ApplicationError to include SaveUseCase and GetCompletedUseCase errors

--- src/application/error.rs
+++ src/application/error.rs
@@ -1,11 +1,7 @@
 use crate::{
     application::usecase::{
-        get_preview_usecase::error::GetPreviewUseCaseError,
-        prepare_segment_usecase::error::PrepareSegmentUseCaseError,
-        redo_usecase::error::RedoUseCaseError, segment_usecase::error::SegmentUseCaseError,
-        undo_usecase::error::UndoUseCaseError, upload_usecase::error::UploadUseCaseError,
-    },
-    parent_error,
+        get_completed_usecase::error::GetCompletedUseCaseError, get_preview_usecase::error::GetPreviewUseCaseError, prepare_segment_usecase::error::PrepareSegmentUseCaseError, redo_usecase::error::RedoUseCaseError, save_usecase::error::SaveUseCaseError, segment_usecase::error::SegmentUseCaseError, undo_usecase::error::UndoUseCaseError, upload_usecase::error::UploadUseCaseError,
+    }, parent_error,
 };
 
 parent_error!(
@@ -15,6 +11,8 @@ parent_error!(
         SegmentUseCase(SegmentUseCaseError),
         UndoUseCase(UndoUseCaseError),
         UploadUseCase(UploadUseCaseError),
+        SaveUseCase(SaveUseCaseError),
+        GetCompletedUseCase(GetCompletedUseCaseError),
         PrepareSegmentUseCase(PrepareSegmentUseCaseError),
     }
 );
