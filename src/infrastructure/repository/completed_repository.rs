52928e5beb6b108e6cc332547ec5d4b251d5524e
ChickRen <chiaking207@icloud.com>
2026-09-15 15:03:17 +0900

    feat: implement deletion of expired images in CompletedRepositoryInMemory

--- src/infrastructure/repository/completed_repository.rs
+++ src/infrastructure/repository/completed_repository.rs
@@ -1,21 +1,18 @@
 use std::{
-    collections::HashMap,
-    sync::{Arc, RwLock},
+    collections::HashMap, ops::Deref, sync::{Arc, RwLock}, time::{Duration, Instant},
 };
 
 use crate::{
     application::{
-        interface::completed_image_repository::{
+        interface::{completed_image_repository::{
             error::CompletedImageRepositoryError, repository::CompletedImageRepository,
-        },
-        types::completed_image::CompletedImage,
-    },
-    domain::value_object::image_id::image_id::ImageId,
+        }, delete_expired_repository::DeleteExpiredRepository}, types::{completed_image::CompletedImage, entry::Entry},
+    }, domain::value_object::image_id::image_id::ImageId,
 };
 
 #[derive(Debug)]
 pub struct CompletedRepositoryInMemory {
-    image: RwLock<HashMap<ImageId, CompletedImage>>,
+    image: RwLock<HashMap<ImageId, Entry<CompletedImage>>>,
 }
 
 impl CompletedImageRepository for CompletedRepositoryInMemory {
@@ -25,7 +22,9 @@ impl CompletedImageRepository for CompletedRepositoryInMemory {
             .write()
             .expect("CompletedImageRepository is Poisoned");
         let image_id = completed_image.image_id();
-        image.insert(image_id, completed_image);
+
+        let entry = Entry::new(completed_image);
+        image.insert(image_id, entry);
     }
 
     fn get(&self, image_id: ImageId) -> Result<CompletedImage, CompletedImageRepositoryError> {
@@ -35,8 +34,18 @@ impl CompletedImageRepository for CompletedRepositoryInMemory {
             .expect("CompletedImageRepository is Poisoned");
         image
             .get(&image_id)
+            .map(|entry| entry.deref())
             .cloned()
             .ok_or(CompletedImageRepositoryError::ImageNotFound)
+    }
+}
+
+impl DeleteExpiredRepository for CompletedRepositoryInMemory {
+    fn delete_expired(&self, now: Instant, ttl: Duration) {
+        let mut image = self.image.write().expect("CompletedImageRepository is Poisoned");
+        image.retain(|_ ,image| {
+            !image.is_expired(now, ttl)
+        });
     }
 }
 
