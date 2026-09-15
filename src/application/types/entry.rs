611f01c569ce0062d683341c0cfb1c8c63257498
ChickRen <chiaking207@icloud.com>
2026-09-15 17:08:33 +0900

    feat: add EntryGuard

--- src/application/types/entry.rs
+++ src/application/types/entry.rs
@@ -1,4 +1,4 @@
-use std::{ops::{Deref, DerefMut}, time::{Duration, Instant}};
+use std::{marker::PhantomData, ops::{Deref, DerefMut}, time::{Duration, Instant}};
 
 #[derive(Debug)]
 pub struct Entry<T> {
@@ -7,16 +7,16 @@ pub struct Entry<T> {
 }
 
 impl<T> Entry<T> {
-    pub fn touch(&mut self) {
-        self.last_accessed = Instant::now()
+    pub fn touch(&mut self, now: Instant) {
+        self.last_accessed = now
     }
 
     pub fn is_expired(&self, now: Instant, ttl: Duration) -> bool {
         (now - self.last_accessed) > ttl
     }
 
-    pub fn new(value: T) -> Self {
-        Self { value, last_accessed: Instant::now() }
+    pub fn new(value: T, now: Instant) -> Self {
+        Self { value, last_accessed: now }
     }
 }
 
@@ -31,5 +31,40 @@ impl<T> Deref for Entry<T> {
 impl<T> DerefMut for Entry<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.value
+    }
+}
+
+pub struct EntryGuard<G, T> {
+    inner: G,
+    phantom: PhantomData<T>
+}
+
+impl<G, T> Deref for EntryGuard<G, T>
+where 
+    G: Deref<Target = Entry<T>>
+{
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.inner.deref()
+    }
+}
+
+impl<G, T> DerefMut for EntryGuard<G, T>
+where
+    G: DerefMut<Target = Entry<T>>
+{
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.inner.deref_mut()
+    }
+}
+
+impl<G, T> EntryGuard<G, T>
+where 
+    G: DerefMut<Target = Entry<T>>
+{
+    pub fn new(mut entry: G, now: Instant) -> Self {
+        entry.touch(now);
+        Self { inner: entry, phantom: PhantomData }
     }
 }
