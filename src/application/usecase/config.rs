c88326d04804680d7e63d8c281b9511fefffd5ed
ChickRen <chiaking207@icloud.com>
2026-09-16 18:56:41 +0900

    feat: add cleanup interval and TTL constants to configuration

--- src/application/usecase/config.rs
+++ src/application/usecase/config.rs
@@ -1,1 +1,5 @@
+use std::time::Duration;
+
 pub const MAX_HISTORY: usize = 20;
+pub const CLEANUP_INTERVAL: Duration = Duration::new(60, 0);
+pub const TTL_SEC: Duration = Duration::new(10 * 60, 0);
