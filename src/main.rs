0a6a72de8f72cc04b8d1468cac4a709fe95a338c
ChickRen <chiaking207@icloud.com>
2026-09-16 19:00:58 +0900

    feat: implement garbage collection task for periodic cleanup

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,41 @@ mod infrastructure;
 mod presentation;
 mod test;
 
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
 use presentation::router;
 use tokio::net::TcpListener;
 
-use crate::composition::wiring::App;
+use crate::{application::{interface::clock::AppClock, usecase::config::CLEANUP_INTERVAL}, composition::wiring::App, infrastructure::clock::RealClock};
 
 #[tokio::main]
 async fn main() {
     let app = Arc::new(App::new().unwrap());
-    let router = router::route(app);
+    let clock = RealClock::new();
+
+    let app_for_gc = app.clone();
+    let app_for_router = app.clone();
+
+    spawn_gc_task(CLEANUP_INTERVAL, move || {
+        app_for_gc.cleanup(clock.now());
+    });
+
+    let router = router::route(app_for_router.clone());
     println!("Server running on http://localhost:3000");
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
+}
+
+fn spawn_gc_task<F>(interval: Duration, task: F)
+where
+    F: Fn() + Send + Sync + 'static
+{
+    tokio::spawn(async move {
+        let mut interval = tokio::time::interval(interval);
+        loop {
+            interval.tick().await;
+            task()
+        }
+    });
 }
