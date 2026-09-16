56b47980f807d30aabcd69a28fc2f065e89a6bd1
ChickRen <chiaking207@icloud.com>
2026-09-16 16:01:09 +0900

    feat: implement constructor for RealClock struct

--- src/infrastructure/clock.rs
+++ src/infrastructure/clock.rs
@@ -2,10 +2,17 @@ use std::time::Instant;
 
 use crate::application::interface::clock::AppClock;
 
+#[derive(Debug, Clone)]
 pub struct RealClock;
 
 impl AppClock for RealClock {
     fn now(&self) -> Instant {
         Instant::now()
+    }
+}
+
+impl RealClock {
+    pub fn new() -> Self {
+        Self
     }
 }
