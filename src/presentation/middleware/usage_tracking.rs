fe8bd5224a091b2b6cd1afeb21c2a120f468c8c6
ChickRen <chiaking207@icloud.com>
2026-09-18 19:37:10 +0900

    feat: implement usage tracking middleware to record operation status

---
+++ src/presentation/middleware/usage_tracking.rs
@@ -0,0 +1,28 @@
+use std::{sync::Arc, time::Instant};
+
+use axum::{extract::{Request, State}, middleware::Next};
+use chrono::Utc;
+
+use crate::{application::{interface::usage_recorder::UsageRecorder, types::usage::{event::UsageEvent, operation::UsageOperation, status::UsageStatus}}, composition::wiring::App, presentation::middleware::extension::UsageStatusExt};
+
+pub async fn track_usage(
+    State(app): State<Arc<App>>,
+    request: Request,
+    next: Next,
+) {
+    let operation = request.uri().path().trim_start_matches("/").to_string();
+    let operation = UsageOperation::new(&operation);
+    let start = Instant::now();
+
+    let response = next.run(request).await;
+
+    let end = start.elapsed();
+
+    let status = match response.extensions().get::<UsageStatusExt>() {
+        None => UsageStatus::Success,
+        Some(err) => UsageStatus::Failed(err.clone().code),
+    };
+
+    let event = UsageEvent::new(operation, status, end, Utc::now());
+    app.usage_recorder().record(event).await;
+}
