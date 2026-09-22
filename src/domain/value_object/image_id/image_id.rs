8460d3c873f473fde424bf4d4cdbb1a90f0a867c
ChickRen <chiaking207@icloud.com>
2026-09-22 16:33:00 +0900

    feat: add ImageId test

--- src/domain/value_object/image_id/image_id.rs
+++ src/domain/value_object/image_id/image_id.rs
@@ -30,3 +30,33 @@ impl ImageId {
         &self.id
     }
 }
+
+#[cfg(test)]
+mod image_id_tests {
+    use uuid::Uuid;
+
+use crate::domain::value_object::image_id::{error::ImageIdError, image_id::ImageId};
+
+    #[test]
+    fn test_normal_convert_from_uuid() {
+        let uuid = Uuid::new_v4();
+        let id = ImageId::from_uuid(uuid);
+
+        assert_eq!(id.value(), &uuid)
+    }
+
+    #[test]
+    fn test_normal_convert_from_str() {
+        let uuid = Uuid::parse_str("1754ca13-617a-46be-bad2-a83eaedccb77");
+        let id = ImageId::from_str("1754ca13-617a-46be-bad2-a83eaedccb77");
+
+        assert_eq!(id.unwrap().value(), &uuid.unwrap())
+    }
+
+    #[test]
+    fn test_invalid_str() {
+        let id = ImageId::from_str("asga129470tgiaehdsg9g8sph");
+
+        assert_eq!(id, Err(ImageIdError::InvalidImageId))
+    }
+}
